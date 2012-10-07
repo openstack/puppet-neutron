dd431e20f21356e313ae8557fc887cfa5eede007
Endre Karlson <endre.karlson@gmail.com>
2012-10-07 01:12:53 +0000

    Add in all config options and make it usable.

--- manifests/plugins/ovs.pp
+++ manifests/plugins/ovs.pp
@@ -1,18 +1,31 @@
-class quantum::plugins::openvswitch (
-  $bridge_uplinkgs      = ['br-virtual:eth1'],
+class quantum::plugins::ovs (
+  $bridge_uplinks      = ['br-virtual:eth1'],
   $bridge_mappings      = ['default:br-virtual'],
+  $tenant_network_type  = "vlan",
+
+  $network_vlan_ranges  = "default:1000:2000",
   $integration_bridge   = "br-int",
-  $server               = true
+
+  $enable_tunneling    = "True",
+  $tunnel_bridge        = "br-tun",
+  $tunnel_id_ranges     = "1:1000",
+  $local_ip             = "10.0.0.1",
+
+  $server               = false
 ) inherits quantum {
   include "quantum::params"
 
   if !$server {
     $package = $::quantum::params::ovs_agent_package
-    $package_require = [Class['quantum'], Service[$::quantum::params::ovs_agent_service]]
+    $package_require = [Class['quantum']]
   } else {
     $package = $::quantum::params::ovs_server_package
-    $package_require = [Class['quantum'], Service[$::quantum::params::ovs_server_service]]
+    $package_require = [Class['quantum'], Service[$::quantum::params::server_service]]
   }
+
+  Package["quantum-plugin-ovs"] -> Quantum_plugin_ovs<||>
+  Quantum_config<||> ~> Service["quantum-plugin-ovs-service"]
+  Quantum_plugin_ovs<||> ~> Service["quantum-plugin-ovs-service"]
 
   class {
     "vswitch":
@@ -24,25 +37,46 @@ class quantum::plugins::openvswitch (
     ensure       => present
   }
 
-  $::quantum::plugins::ovs::bridge{$bridge_mappings: }
+  quantum::plugins::ovs::bridge{$bridge_mappings:}
 
-  $::quantum::plugins::ovs::port{$bridge_uplinks: }
+  quantum::plugins::ovs::port{$bridge_uplinks:}
 
-  package { "quantum-plugin-openvswitch":
+  package { "quantum-plugin-ovs":
     name    => $package,
-    ensure  => latest,
+    ensure  => $package_ensure,
     require => $package_require
   }
 
+  $br_map_str = join($bridge_mappings, ",")
   quantum_plugin_ovs {
-    bridge_mappings:    value     => $bridge_mappings,
-    integration_bridge: value     => $integration_bridge
+    "OVS/integration_bridge":   value => $integration_bridge;
+    "OVS/network_vlan_ranges":  value => $network_vlan_ranges;
+    "OVS/tenant_network_type":  value => $tenant_network_type;
+    "OVS/bridge_mappings":      value => $br_map_str;
   }
 
-  service { 'quantum-ovs-service-agent':
+  if ($tenant_network_type == "gre") and ($enable_tunneling) {
+    vs_bridge {$tunnel_bridge:
+      ensure => present
+    }
+    quantum_plugin_ovs {
+      "OVS/enable_tunneling":   value => $enable_tunneling;
+      "OVS/tunnel_bridge":      value => $tunnel_bridge;
+      "OVS/tunnel_id_ranges":   value => $tunnel_id_ranges;
+      "OVS/local_ip":           value => $local_ip;
+    }
+  }
+
+  if $enabled {
+    $service_ensure = "running"
+  } else {
+    $service_ensure = "stopped"
+  }
+
+  service { 'quantum-plugin-ovs-service':
     name    => $::quantum::params::ovs_agent_service,
-    enable  => true,
-    ensure  => running,
+    enable  => $enable,
+    ensure  => $service_ensure,
     require => [Package[$package]]
   }
 }

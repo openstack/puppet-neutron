71d5a6ef73988638393fd7cd2866e5b5b9ff3131
Endre Karlson <endre.karlson@gmail.com>
2012-10-07 00:00:47 +0000

    Change in regards to dependencies.
    Re-organize so that we don't need to install quantum_server at all.
    Rename br-ext to br-virtual.
    Move vs_* providers out if the if.
    Add requires for package

--- manifests/plugins/openvswitch.pp
+++ manifests/plugins/openvswitch.pp
@@ -1,43 +1,49 @@
 class quantum::plugins::openvswitch (
   $private_bridge       = 'br-int',
   $private_interface    = 'eth0',
-  $public_bridge        = 'br-ext',
+  $public_bridge        = 'br-virtual',
   $public_interface     = 'eth1',
-  $openvswitch_settings = false,
+  $plugin_settings      = false,
   $controller           = true
 ) {
   include "quantum::params"
 
+  notice("Controller $controller")
 
-  if $controller {
+  if !$controller {
     $package = $::quantum::params::ovs_package_agent
-
-    class {
-      "vswitch":
-        provider => ovs
-    }
-
-    vs_bridge {$private_bridge:
-      external_ids => "bridge-id=$private_bridge"
-    }
-
-    vs_port {$private_interface:
-      bridge  => $private_bridge
-    }
-
-    vs_bridge {$public_bridge:
-      external_ids => "bridge-id=$public_bridge"
-    }
-
-    vs_port {$public_interface:
-      bridge => $public_bridge
-    }
+    $package_require = Service[$::quantum::params::ovs_service]
   } else {
     $package = $::quantum::params::ovs_package_server
+    $package_require = [Class['quantum'], Service[$::quantum::params::ovs_service]]
   }
 
+  class {
+    "vswitch":
+      provider => ovs
+  }
 
-  package { $package: }
+  vs_bridge {$private_bridge:
+    external_ids => "bridge-id=$private_bridge"
+  }
+
+  vs_port {$private_interface:
+    bridge  => $private_bridge
+  }
+
+  vs_bridge {$public_bridge:
+    external_ids => "bridge-id=$public_bridge"
+  }
+
+  vs_port {$public_interface:
+    bridge => $public_bridge
+  }
+
+  package { "quantum-plugin-openvswitch":
+    name    => $package,
+    ensure  => latest,
+    require => $package_require
+  }
 
   File {
     require => Package['quantum-plugin-openvswitch'],

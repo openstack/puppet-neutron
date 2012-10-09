e249612abb10a334a3d0752e6ee3fb524a47e55d
Endre Karlson <endre.karlson@gmail.com>
2012-10-09 09:56:04 +0200

    New and shiny manifest for commons

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,75 +1,82 @@
 class quantum (
-  $keystone_password,
-  $quantum_settings       = false,
-  $keystone_enabled       = true,
-  $keystone_tenant        = 'services',
-  $keystone_user          = 'quantum',
-  $keystone_auth_host     = 'localhost',
-  $keystone_auth_port     = '35357',
-  $keystone_auth_protocol = 'http',
-  $package_ensure         = 'latest',
-  $enabled                = true,
-) {
+  $ensure               = true,
+  $log_verbose          = "False",
+  $log_debug            = "False",
 
+  $bind_host            = "0.0.0.0",
+  $bind_port            = "9696",
+  $sql_connection       = "sqlite:///var/lib/quantum/quantum.sqlite",
+
+  $auth_type            = "keystone",
+  $auth_host            = "localhost",
+  $auth_port            = "35357",
+  $auth_uri             = "http://localhost:5000",
+  $keystone_tenant      = "service",
+  $keystone_user        = "quantum",
+  $keystone_password    = "ChangeMe",
+
+  $rabbit_host          = "localhost",
+  $rabbit_port          = "5672",
+  $rabbit_user          = "guest",
+  $rabbit_password      = "guest",
+  $rabbit_virtual_host  = "/",
+
+  $control_exchange     = "quantum",
+
+  $core_plugin            = "quantum.plugins.openvswitch.ovs_quantum_plugin.OVSQuantumPluginV2",
+  $mac_generation_retries = 16,
+  $dhcp_lease_duration    = 120
+) {
   include quantum::params
 
-  package { 'quantum':
-    name   => $::quantum::params::package_name,
-    ensure => $package_ensure,
-  }
+  validate_re($sql_connection, '(sqlite|mysql|posgres):\/\/(\S+:\S+@\S+\/\S+)?')
 
-  package { 'python-cliff':
-    name   => $::quantum::params::cliff_name,
-    ensure => latest,
-  }
+  Package['quantum'] -> Quantum_config<||>
 
-  File {
-    ensure  => present,
-    owner   => 'quantum',
-    group   => 'quantum',
-    mode    => '0644',
-    require => Package[$::quantum::params::package_name],
-    notify  => Service[$::quantum::params::service_name],
-  }
-
-  file { $::quantum::params::quantum_conf: }
-  file { $::quantum::params::quantum_paste_api_ini: }
-
-  if $quantum_settings {
-    multini($::quantum::params::quantum_conf, $quantum_settings)
-  }
-
-  if $keystone_enabled {
-    multini($::quantum::params::quantum_conf, { 'DEFAULT' => { 'auth_strategy' => 'keystone' } })
-    $keystone_settings = {
-      'filter:authtoken' => {
-        'auth_host'         => $keystone_auth_host,
-        'auth_port'         => $keystone_auth_port,
-        'auth_protocol'     => $keystone_auth_protocol,
-        'admin_user'        => $keystone_user,
-        'admin_password'    => $keystone_password,
-        'admin_tenant_name' => $keystone_tenant
-      }
-    }
- 
-    multini($::quantum::params::quantum_paste_api_ini, $keystone_settings)
- 
-  }
-
-  if $enabled {
-    $ensure = 'running'
+  if ($sql_connection =~ /mysql:\/\/\S+:\S+@\S+\/\S+/) {
+    ensure_resource( 'package', 'python-mysqldb', {'ensure' => 'present'})
+  } elsif ($sql_connection =~ /postgresql:\/\/\S+:\S+@\S+\/\S+/) {
+    ensure_resource( 'package', 'python-psycopg2', {'ensure' => 'present'})
+  } elsif($sql_connection =~ /sqlite:\/\//) {
+    ensure_resource( 'package', 'python-pysqlite2', {'ensure' => 'present'})
   } else {
-    $ensure = 'stopped'
+    fail("Invalid db connection ${sql_connection}")
   }
 
-  service { 'quantum':
-    name    => $::quantum::params::service_name,
-    enable  => $enabled,
-    ensure  => $ensure,
-    require => Package[$::quantum::params::package_name],
+  file {"/etc/quantum":
+    ensure  => directory,
+    owner   => "quantum",
+    group   => "root",
+    mode    => 770,
+    require => Package["quantum"]
   }
 
-  Ini_setting<| tag == $::quantum::params::quantum_conf_tag |> ~> Service['quantum']
-  Ini_setting<| tag == $::quantum::params::quantum_paste_api_ini_tag |> ~> Service['quantum']
+  package {"quantum":
+    name   => $::quantum::params::package_name,
+    ensure => $ensure
+  }
 
+  quantum_config {
+    "DEFAULT/verbose":    value => $log_verbose;
+    "DEFAULT/debug":      value => $log_debug;
+
+    "DEFAULT/bind_host":  value => $bind_host;
+    "DEFAULT/bind_port":  value => $bind_port;
+
+    "DEFAULT/sql_connection":       value => $sql_connection;
+
+    "DEFAULT/auth_strategy":        value => $auth_strategy;
+
+    "DEFAULT/rabbit_host":          value => $rabbit_host;
+    "DEFAULT/rabbit_port":          value => $rabbit_port;
+    "DEFAULT/rabbit_user":          value => $rabbit_user;
+    "DEFAULT/rabbit_password":      value => $rabbit_password;
+    "DEFAULT/rabbit_virtual_host":  value => $rabbit_password;
+
+    "DEFAULT/control_exchange":     value => $control_exchange;
+
+    "DEFAULT/core_plugin":            value => $core_plugin;
+    "DEFAULT/mac_generation_retries": value => $mac_generation_retries;
+    "DEFAULT/dhcp_lease_duration":    value => $dhcp_lease_duration;
+  }
 }

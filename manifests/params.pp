285f747a563f440f87401b89d111f8d8b017ef9d
Endre Karlson <endre.karlson@gmail.com>
2012-10-09 10:26:41 +0200

    Remove - unneeded

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,17 +1,4 @@
 class quantum::params {
-  $quantum_conf = '/etc/quantum/quantum.conf'
-  $quantum_conf_tag = regsubst($quantum_conf, '/', '_', 'G')
-  $quantum_paste_api_ini = '/etc/quantum/api-paste.ini'
-  $quantum_paste_api_ini_tag = regsubst($quantum_paste_api_ini, '/', '_', 'G')
-
-  $quantum_dhcp_agent_ini = '/etc/quantum/dhcp_agent.ini'
-  $quantum_dhcp_agent_ini_tag = regsubst($quantum_dhcp_agent_ini, '/', '_', 'G')
-  $quantum_l3_agent_ini   = '/etc/quantum/l3_agent.ini'
-  $quantum_l3_agent_ini_tag = regsubst($quantum_l3_agent_ini, '/', '_', 'G')
-
-  $quantum_ovs_plugin_ini = '/etc/quantum/plugins/openvswitch/ovs_quantum_plugin.ini'
-  $quantum_ovs_plugin_ini_tag = regsubst($quantum_ovs_plugin_ini, '/', '_', 'G') 
-
   case $::osfamily {
     'Debian', 'Ubuntu': {
       $package_name       = 'quantum-common'

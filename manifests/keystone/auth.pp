a180cd379854001e1cdf21b019087eec9d0d84e9
Dan Bode <dan@puppetlabs.com>
2012-11-01 13:19:32 -0700

    update keystone_endpoints  keystone folsom module
    The title of the keystone module for folsom has
    changed. This updates the config for keystone_endpoints
    to include region as a part of the namevar.

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -31,9 +31,8 @@ class quantum::keystone::auth (
   }
 
   if $configure_endpoint {
-    keystone_endpoint { $auth_name:
+    keystone_endpoint { "${region}/$auth_name":
       ensure       => present,
-      region       => $region,
       public_url   => "http://${public_address}:${port}",
       admin_url    => "http://${admin_address}:${port}",
       internal_url => "http://${internal_address}:${port}",

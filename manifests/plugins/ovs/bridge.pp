1d5971fd1188043e54de8ebff922a8f109fa8a7d
Dan Bode <bodepd@gmail.com>
2013-04-19 15:00:00 -0700

    switch from deprecated defined resource types
    the defined resource types vswitch::bridge and vswitch::port
    have been deleted b/c they didnt actually do anything (other
    than create a worthless abstraction layer)
    
    This commit makes the appropriate changes to stop using them in
    quantum.

--- manifests/plugins/ovs/bridge.pp
+++ manifests/plugins/ovs/bridge.pp
@@ -2,7 +2,7 @@ define quantum::plugins::ovs::bridge {
   $mapping = split($name, ":")
   $bridge = $mapping[1]
 
-  vswitch::bridge {$bridge:
+  vs_bridge {$bridge:
     ensure       => present,
     external_ids => "bridge-id=${bridge}"
   }
--- manifests/plugins/ovs/port.pp
+++ manifests/plugins/ovs/port.pp
@@ -1,6 +1,6 @@
 define quantum::plugins::ovs::port {
   $mapping = split($name, ":")
-  vswitch::port {$mapping[1]:
+  vs_port {$mapping[1]:
     ensure => present,
     bridge => $mapping[0]
   }

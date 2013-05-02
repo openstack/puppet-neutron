c28a656557b8544a7d10c73de17619ed5560dc86
Mehdi Abaakouk <mehdi.abaakouk@enovance.com>
2013-05-02 15:04:23 +0200

    Allow changed to public_protocol and public_port
    This change allow to configure the public_protocol and public_port of
    quantum endpoint and add tests for the keystone/auth.pp file
    
    Change-Id: I0e643d60bde99a0524944b7ed96d445ba63300fe

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -5,14 +5,22 @@ class quantum::keystone::auth (
   $tenant             = 'services',
   $configure_endpoint = true,
   $service_type       = 'network',
+  $public_protocol    = 'http',
   $public_address     = '127.0.0.1',
   $admin_address      = '127.0.0.1',
   $internal_address   = '127.0.0.1',
   $port               = '9696',
+  $public_port        = undef,
   $region             = 'RegionOne'
 ) {
 
   Keystone_user_role["${auth_name}@services"] ~> Service <| name == 'quantum-server' |>
+
+  if ! $public_port {
+    $real_public_port = $port
+  } else {
+    $real_public_port = $public_port
+  }
 
   keystone_user { $auth_name:
     ensure   => present,
@@ -33,9 +41,9 @@ class quantum::keystone::auth (
   if $configure_endpoint {
     keystone_endpoint { "${region}/${auth_name}":
       ensure       => present,
-      public_url   => "http://${public_address}:${port}/",
-      admin_url    => "http://${admin_address}:${port}/",
+      public_url   => "${public_protocol}://${public_address}:${real_public_port}/",
       internal_url => "http://${internal_address}:${port}/",
+      admin_url    => "http://${admin_address}:${port}/",
     }
 
   }

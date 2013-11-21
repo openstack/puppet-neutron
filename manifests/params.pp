32f62889e09cbbdaad84d0d612a7c339d7947328
root <root@os-havana-ctr.plumgrid.com>
2013-11-21 19:52:11 +0500

    Disabled OVS neutron plugin and configured plumgrid neutron plugin.

--- manifests/params.pp
+++ manifests/params.pp
@@ -64,6 +64,8 @@ class neutron::params {
     $ovs_server_package  = 'neutron-plugin-openvswitch'
     $ovs_cleanup_service = false
 
+    $plumgrid_plugin_package = 'neutron-plugin-plumgrid'
+
     $linuxbridge_agent_package  = 'neutron-plugin-linuxbridge-agent'
     $linuxbridge_agent_service  = 'neutron-plugin-linuxbridge-agent'
     $linuxbridge_server_package = 'neutron-plugin-linuxbridge'

9004922a47bcdffa4739c3664b68d66e03265ea3
salmank <salmank@plumgrid.com>
2013-11-24 19:13:49 +0500

    Modify /etc/default/neutron-server to map on /etc/neutron/plugins/plumgrid/plumgrid.ini Changed /etc/neutron/plugins/plumgrid/plumgrid.ini configuration.

--- manifests/plugins/plumgrid.pp
+++ manifests/plugins/plumgrid.pp
@@ -17,20 +17,35 @@ class neutron::plugins::plumgrid (
   Package['neutron'] -> Package['neutron-plugin-plumgrid']
   Package['neutron-plugin-plumgrid'] -> Neutron_plugin_plumgrid<||>
   Neutron_plugin_plumgrid<||> ~> Service<| title == 'neutron-server' |>
-  Package['neutron-plugin-plumgrid'] -> Service<| title == 'neutron-server' |>
+  Package['neutron-plugin-plumgrid'] -> File['remove plumgrid.ini']
+  File['remove plumgrid.ini'] -> Service<| title == 'neutron-server' |>
+
+  file_line { '/etc/default/neutron-server:NEUTRON_PLUGIN_CONFIG':
+      path    => '/etc/default/neutron-server',
+      match   => '^NEUTRON_PLUGIN_CONFIG=(.*)$',
+      line    => "NEUTRON_PLUGIN_CONFIG=${::neutron::params::plumgrid_config_file}",
+      require => [ Package['neutron-server'], Package['neutron-plugin-plumgrid'] ],
+      notify  => Service['neutron-server'],
+    }
 
   package { 'neutron-plugin-plumgrid':
     ensure  => $package_ensure,
     name    => $::neutron::params::plumgrid_plugin_package,
   }
 
+  file { 'remove plumgrid.ini':
+      path => '/etc/neutron/plugins/plumgrid/plumgrid.ini',
+      ensure => absent,
+      require => [Package['neutron-plugin-plumgrid'] ],
+  }
+
   neutron_plugin_plumgrid {
-    'PLUMGRID/connection': value => $connection;
-    'PLUMGRID/director_server': value => $pg_director_server;
-    'PLUMGRID/director_server_port' : value => $pg_director_server_port;
-    'PLUMGRID/username': value => $pg_username;
-    'PLUMGRID/password': value => $pg_password;
-    'PLUMGRID/servertimeout': value => $pg_servertimeout;
+    'PLUMgridDirector/director_server': value => $pg_director_server;
+    'PLUMgridDirector/director_server_port' : value => $pg_director_server_port;
+    'PLUMgridDirector/username': value => $pg_username;
+    'PLUMgridDirector/password': value => $pg_password;
+    'PLUMgridDirector/servertimeout': value => $pg_servertimeout;
+    'database/connection': value => $connection;
   }
 
 }

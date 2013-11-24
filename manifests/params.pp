d5d3643f0a69e9e58b5ab9cdccc3e038822bcdc0
salmank <salmank@plumgrid.com>
2013-11-24 19:13:49 +0500

    Modify /etc/default/neutron-server to map on /etc/neutron/plugins/plumgrid/plumgrid.ini Changed /etc/neutron/plugins/plumgrid/plumgrid.ini configuration.

--- manifests/params.pp
+++ manifests/params.pp
@@ -56,6 +56,7 @@ class neutron::params {
     $ovs_cleanup_service = false
 
     $plumgrid_plugin_package = 'neutron-plugin-plumgrid'
+    $plumgrid_config_file = '/etc/neutron/plugins/plumgrid/plumgrid.ini'
 
     $linuxbridge_agent_package  = 'neutron-plugin-linuxbridge-agent'
     $linuxbridge_agent_service  = 'neutron-plugin-linuxbridge-agent'

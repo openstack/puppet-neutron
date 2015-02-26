208968bc18f64af8c801bbf63247cfa5f8ff8b61
Lukas Bezdicka <lbezdick@redhat.com>
2015-02-26 12:13:00 -0500

    Fix neutron_network for --router:external setting
    Neutron can't be called with --router:external=True as that
    will cause "ignored explicit argument u'True'" error.
    
    Change-Id: I49a033fd2821100283e217225896fc93a927edd2
    (cherry picked from commit b29ee6f706bf7371f69b831f99727396ddb77cad)

--- lib/puppet/provider/neutron_network/neutron.rb
+++ lib/puppet/provider/neutron_network/neutron.rb
@@ -80,8 +80,8 @@ Puppet::Type.type(:neutron_network).provide(
         "--provider:segmentation_id=#{@resource[:provider_segmentation_id]}"
     end
 
-    if @resource[:router_external]
-      network_opts << "--router:external=#{@resource[:router_external]}"
+    if @resource[:router_external] == 'True'
+      network_opts << '--router:external'
     end
 
     results = auth_neutron('net-create', '--format=shell',
@@ -120,7 +120,11 @@ Puppet::Type.type(:neutron_network).provide(
   end
 
   def router_external=(value)
-    auth_neutron('net-update', "--router:external=#{value}", name)
+    if value == 'False'
+      auth_neutron('net-update', "--router:external=#{value}", name)
+    else
+      auth_neutron('net-update', "--router:external", name)
+    end
   end
 
   [

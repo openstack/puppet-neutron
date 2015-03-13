63130cd92ccc3d0f9f40603d28bc48178412e481
Lukas Bezdicka <lbezdick@redhat.com>
2015-03-13 13:06:08 +0100

    Fix ipv6 support
    There is issue with ipv6 where address has to be in brackets, this causes the
    underlying ruby TCPSocket to fail. Net::HTTP.new will fail without brackets on
    joining the ipv6 address with :port or passing brackets to TCPSocket. It was
    found that if we use Net::HTTP.start with url.hostname the incriminated code
    won't be hit.
    
    Change-Id: I99bbb8b7aaeae8958f757772ba6186f1e48a6778
    Closes-Bug: rhbz#1185652
    (cherry picked from commit c852fcdec66bfb894f3980685fece07ec3b6917c)

--- lib/puppet/provider/nova_admin_tenant_id_setter/ini_setting.rb
+++ lib/puppet/provider/nova_admin_tenant_id_setter/ini_setting.rb
@@ -32,8 +32,13 @@ end
 #   A parsed URL (returned from URI.parse)
 def handle_request(req, url)
     begin
-        http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = url.scheme == 'https'
+        # There is issue with ipv6 where address has to be in brackets, this causes the
+        # underlying ruby TCPSocket to fail. Net::HTTP.new will fail without brackets on
+        # joining the ipv6 address with :port or passing brackets to TCPSocket. It was
+        # found that if we use Net::HTTP.start with url.hostname the incriminated code
+        # won't be hit.
+        use_ssl = url.scheme == "https" ? true : false
+        http = Net::HTTP.start(url.hostname, url.port, {:use_ssl => use_ssl})
         res = http.request(req)
 
         if res.code != '200'

5e034e2af7ecb31cfcb758c7f43f47e46ce5677a
Jaume Devesa <devvesa@gmail.com>
2015-03-17 16:32:51 +0000

    Add MidoNet plugin manifest
    Define the resources to configure MidoNet plugin with Neutron. Is worth
    say that, since MidoNet plugin is not in official repos, this manifest
    does not install the packages and delegates to an orchestration tool or
    a wrapper manifest to do so.
    
    Is is planned to add package declaration after Kilo release.
    
    Change-Id: Ib5f73729f314a40197b4d1c75ec33fd246df83d9

--- manifests/params.pp
+++ manifests/params.pp
@@ -31,6 +31,9 @@ class neutron::params {
     $cisco_config_file     = '/etc/neutron/plugins/cisco/cisco_plugins.ini'
     $cisco_ml2_config_file = '/etc/neutron/plugins/ml2/ml2_conf_cisco.ini'
 
+    $midonet_server_package = 'python-neutron-plugin-midonet'
+    $midonet_config_file    = '/etc/neutron/plugins/midonet/midonet.ini'
+
     $nvp_server_package = 'openstack-neutron-nicira'
 
     $dhcp_agent_package = false
@@ -99,6 +102,9 @@ class neutron::params {
     $cisco_server_package  = 'neutron-plugin-cisco'
     $cisco_config_file     = '/etc/neutron/plugins/cisco/cisco_plugins.ini'
     $cisco_ml2_config_file = '/etc/neutron/plugins/ml2/ml2_conf_cisco.ini'
+
+    $midonet_server_package = 'python-neutron-plugin-midonet'
+    $midonet_config_file    = '/etc/neutron/plugins/midonet/midonet.ini'
 
     $nvp_server_package = 'neutron-plugin-nicira'
 

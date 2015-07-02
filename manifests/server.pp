d9d6f2ae3860dd8353dec423d6717b51dcc7c37f
Alex Schultz <aschultz@mirantis.com>
2015-07-02 16:12:46 -0500

    Add ability to specify auth_region
    This change adds the ability to specify auth_region in neutron.conf.
    
    Change-Id: I475ba151a1fe841b3f84e301a9e68fdf05f5afa4
    Closes-Bug: 1471018

--- manifests/server.pp
+++ manifests/server.pp
@@ -52,6 +52,11 @@
 #   (where '/keystone' is the admin prefix)
 #   Defaults to false for empty. If defined, should be a string with a leading '/' and no trailing '/'.
 #
+# [*auth_region*]
+#   (optional) The authentication region. Note this value is case-sensitive and
+#   must match the endpoint region defined in Keystone.
+#   Defaults to undef
+#
 # [*auth_tenant*]
 #   (optional) The tenant of the auth user
 #   Defaults to services
@@ -196,6 +201,7 @@ class neutron::server (
   $manage_service                   = true,
   $service_name                     = $::neutron::params::server_service,
   $auth_password                    = false,
+  $auth_region                      = undef,
   $auth_type                        = 'keystone',
   $auth_tenant                      = 'services',
   $auth_user                        = 'neutron',
@@ -469,6 +475,12 @@ class neutron::server (
       }
       neutron_api_config {
         'filter:authtoken/auth_uri': value => $auth_uri_real;
+      }
+
+      if $auth_region {
+        neutron_config {
+          'keystone_authtoken/auth_region': value => $auth_region;
+        }
       }
 
       if $identity_uri {

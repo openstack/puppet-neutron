38126af0076be39a9b9362312494b0d9cfdcf6a2
Jiri Stransky <jistr@redhat.com>
2015-08-19 14:52:21 +0200

    Configure Big Switch ML2 plugin
    Adds a class to install the Big Switch ML2 plugin and another one to
    configure the [restproxy] section of ml2_conf.ini. Currently, only
    RedHat osfamily is supported.
    
    Change-Id: If027762f70695c31dbca6151373a0ebe6e75d071
    (cherry picked from commit f8ec8a20a0137ac15d7a6303d46fd9c8f3fbd9d7)

---
+++ spec/classes/neutron_plugins_ml2_bigswitch_restproxy_spec.rb
@@ -0,0 +1,78 @@
+#
+# Unit tests for neutron::plugins::ml2::cisco::nexus class
+#
+
+require 'spec_helper'
+
+describe 'neutron::plugins::ml2::bigswitch::restproxy' do
+
+  let :pre_condition do
+    "class { 'neutron::server': auth_password => 'password'}
+     class { 'neutron':
+      rabbit_password => 'passw0rd',
+      core_plugin     => 'neutron.plugins.ml2.plugin.Ml2Plugin' }"
+  end
+
+  let :required_params do
+    {
+      :servers     => '192.168.0.10:8000,192.168.0.11:8000',
+      :server_auth => 'admin:password',
+    }
+  end
+
+  let :params do
+    required_params
+  end
+
+  let :default_facts do
+    { :operatingsystem         => 'default',
+      :operatingsystemrelease  => 'default',
+    }
+  end
+
+  shared_examples_for 'neutron bigswitch ml2 restproxy' do
+
+    it { is_expected.to contain_class('neutron::params') }
+    it { is_expected.to contain_class('neutron::plugins::ml2::bigswitch') }
+
+    it do
+      is_expected.to contain_neutron_plugin_ml2('restproxy/servers').with_value(params[:servers])
+      is_expected.to contain_neutron_plugin_ml2('restproxy/server_auth').with_value(params[:server_auth])
+
+      is_expected.to contain_neutron_plugin_ml2('restproxy/auto_sync_on_failure').with_value(true)
+      is_expected.to contain_neutron_plugin_ml2('restproxy/consistency_interval').with_value(60)
+      is_expected.to contain_neutron_plugin_ml2('restproxy/neutron_id').with_value('neutron')
+      is_expected.to contain_neutron_plugin_ml2('restproxy/server_ssl').with_value(true)
+      is_expected.to contain_neutron_plugin_ml2('restproxy/ssl_cert_directory').with_value('/var/lib/neutron')
+    end
+
+    context 'with custom params' do
+      let :params do
+        required_params.merge({
+          :auto_sync_on_failure => false,
+          :consistency_interval => 10,
+          :neutron_id           => 'openstack',
+          :server_ssl           => false,
+          :ssl_cert_directory   => '/var/lib/bigswitch',
+        })
+      end
+
+      it do
+        is_expected.to contain_neutron_plugin_ml2('restproxy/auto_sync_on_failure').with_value(false)
+        is_expected.to contain_neutron_plugin_ml2('restproxy/consistency_interval').with_value(10)
+        is_expected.to contain_neutron_plugin_ml2('restproxy/neutron_id').with_value('openstack')
+        is_expected.to contain_neutron_plugin_ml2('restproxy/server_ssl').with_value(false)
+        is_expected.to contain_neutron_plugin_ml2('restproxy/ssl_cert_directory').with_value('/var/lib/bigswitch')
+      end
+    end
+
+  end
+
+  context 'on RedHat platforms' do
+    let :facts do
+      default_facts.merge({:osfamily => 'RedHat'})
+    end
+
+    it_configures 'neutron bigswitch ml2 restproxy'
+  end
+end
---
+++ spec/classes/neutron_plugins_ml2_bigswitch_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+
+describe 'neutron::plugins::ml2::bigswitch' do
+
+  let :pre_condition do
+    "class { 'neutron::server': auth_password => 'password'}
+     class { 'neutron':
+      rabbit_password => 'passw0rd',
+      core_plugin     => 'neutron.plugins.ml2.plugin.Ml2Plugin' }"
+  end
+
+  let :default_params do
+    {
+      :package_ensure => 'present'
+    }
+  end
+
+  let :params do
+    {}
+  end
+
+  let :default_facts do
+    {
+      :operatingsystem        => 'default',
+      :operatingsystemrelease => 'default',
+    }
+  end
+
+
+  shared_examples_for 'neutron plugin bigswitch ml2' do
+    before do
+      params.merge!(default_params)
+    end
+
+    it { is_expected.to contain_class('neutron::params') }
+
+    it 'should have' do
+      is_expected.to contain_package('python-networking-bigswitch').with(
+        :ensure => params[:package_ensure],
+        :tag    => 'openstack'
+        )
+    end
+  end
+
+  context 'on RedHat platforms' do
+    let :facts do
+      default_facts.merge({:osfamily => 'RedHat'})
+    end
+
+    it_configures 'neutron plugin bigswitch ml2'
+  end
+
+  context 'on Debian platforms' do
+    let :facts do
+      default_facts.merge({:osfamily => 'Debian'})
+    end
+
+    it { is_expected.to raise_error(Puppet::Error, /Unsupported osfamily Debian/) }
+  end
+end

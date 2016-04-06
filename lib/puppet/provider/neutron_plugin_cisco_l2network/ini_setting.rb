583ea057de3935f7b272f984625de41a73bd1db6
Nathan A. Taylor <ntaylor1781@gmail.com>
2016-04-06 13:01:17 -0600

    Set ini/openstack libs to pass file_path properly
    Currently the file_path sections, of the ini_setting.rb's and
    openstackconfig.rb's, are not being passed in the best way. When trying
    to use ini_settings it throws an error stating "ini_settings only
    support collecting instances when a file path is hard coded". Passing
    the variables with the 'self.' alleviates this.
    
    This is needed in order to be able to use the purge_config option.
    
    Conflicts:
    	lib/puppet/provider/neutron_lbaas_service_config/openstackconfig.rb
    
      *Added backwards compatability for older versions of inifile
    
    Change-Id: I9b09bf574208af5c044508129e41ec7420ac1f82
    (cherry picked from commit 39698c4d6282831a4030ccfb2cf1353c27aff50f)

--- lib/puppet/provider/neutron_plugin_cisco_l2network/ini_setting.rb
+++ lib/puppet/provider/neutron_plugin_cisco_l2network/ini_setting.rb
@@ -3,8 +3,13 @@ Puppet::Type.type(:neutron_plugin_cisco_l2network).provide(
   :parent => Puppet::Type.type(:openstack_config).provider(:ini_setting)
 ) do
 
-  def file_path
+  def self.file_path
     '/etc/neutron/plugins/cisco/l2network_plugin.ini'
+  end
+
+  # added for backwards compatibility with older versions of inifile
+  def file_path
+    self.class.file_path
   end
 
 end

cd1f0d98ccb3cfe877a6238f85cd1bf68f4b2011
ZhongShengping <chdzsp@163.com>
2016-06-04 00:27:48 +0800

    Test multiple operating systems for policy
    Use on_supported_os function to test multiple
    operating systems for class neutron::policy.
    
    Change-Id: I6c486fccc5160d3418b5b5c8631158d7f2d7f350

--- spec/classes/neutron_policy_spec.rb
+++ spec/classes/neutron_policy_spec.rb
@@ -33,24 +33,16 @@ describe 'neutron::policy' do
     end
   end
 
-  context 'on Debian platforms' do
-    let :facts do
-      @default_facts.merge(test_facts.merge({
-         :osfamily => 'Debian'
-      }))
-    end
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge(OSDefaults.get_facts({:processorcount => 8}))
+      end
 
-    it_configures 'neutron policies'
+      it_configures 'neutron policies'
+    end
   end
 
-  context 'on RedHat platforms' do
-    let :facts do
-      @default_facts.merge(test_facts.merge({
-         :osfamily               => 'RedHat',
-         :operatingsystemrelease => '7'
-      }))
-    end
-
-    it_configures 'neutron policies'
-  end
 end

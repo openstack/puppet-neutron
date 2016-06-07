f0d888ac91d9d3edad87b8bb36b7d0339af7fabf
iberezovskiy <iberezovskiy@mirantis.com>
2016-06-07 19:56:43 +0300

    Fix facts for policy test
    Change-Id: I460e2656ae57016824ec8635f7995b5d00fe0ff2

--- spec/classes/neutron_policy_spec.rb
+++ spec/classes/neutron_policy_spec.rb
@@ -38,7 +38,7 @@ describe 'neutron::policy' do
   }).each do |os,facts|
     context "on #{os}" do
       let (:facts) do
-        facts.merge(OSDefaults.get_facts({:processorcount => 8}))
+        facts.merge!(OSDefaults.get_facts())
       end
 
       it_configures 'neutron policies'

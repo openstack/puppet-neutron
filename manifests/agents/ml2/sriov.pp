b20ea67cd40849eaa1e4996282ab32fb9b43b771
karthik s <ksundara@redhat.com>
2016-07-18 14:12:33 +0530

    Disable firewall for SR-IOV ports
    The SR-IOV nic agent shall be configured with NOOP firewall driver
    
    Implements: blueprint tripleo-sriov
    
    Change-Id: I9dfc238903d78a9cc964063c95a149eaaf6059ad
    Signed-off-by: karthik s <ksundara@redhat.com>

--- manifests/agents/ml2/sriov.pp
+++ manifests/agents/ml2/sriov.pp
@@ -85,6 +85,9 @@ class neutron::agents::ml2::sriov (
     'sriov_nic/exclude_devices':          value => pick(join(any2array($exclude_devices), ','), $::os_service_default);
     'sriov_nic/physical_device_mappings': value => pick(join(any2array($physical_device_mappings), ','), $::os_service_default);
     'agent/extensions':                   value => join(any2array($extensions), ',');
+    # As of now security groups are not supported for SR-IOV ports.
+    # It is required to disable Firewall driver in the SR-IOV agent config.
+    'securitygroup/firewall_driver':      value => 'neutron.agent.firewall.NoopFirewallDriver';
   }
 
   package { 'neutron-sriov-nic-agent':

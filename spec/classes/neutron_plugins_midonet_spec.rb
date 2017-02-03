76113ea9451bd02b4bbac6a21e67c3134abb8352
Alejandro Andreu <alejandroandreu@openmailbox.org>
2017-02-03 12:06:19 +0100

    Update MidoNet plugin manifest
    The MidoNet API no longer exists with that name. We are deprecating its
    parameters and replacing them by its counterpart "cluster".
    
    Also the dependency python-networking-midonet-ext package is removed as
    this is only needed for the N release. Will be added when backported.
    
    Change-Id: Ia85bc41c05d766f995835941ed5fc46ce5465334

--- spec/classes/neutron_plugins_midonet_spec.rb
+++ spec/classes/neutron_plugins_midonet_spec.rb
@@ -12,12 +12,12 @@ describe 'neutron::plugins::midonet' do
 
   let :default_params do
   {
-    :midonet_api_ip    => '127.0.0.1',
-    :midonet_api_port  => '8080',
-    :keystone_username => 'neutron',
-    :keystone_password => 'test_midonet',
-    :keystone_tenant   => 'services',
-    :purge_config      => false,
+    :midonet_cluster_ip    => '127.0.0.1',
+    :midonet_cluster_port  => '8181',
+    :keystone_username     => 'neutron',
+    :keystone_password     => 'test_midonet',
+    :keystone_tenant       => 'services',
+    :purge_config          => false,
   }
   end
 
@@ -57,13 +57,33 @@ describe 'neutron::plugins::midonet' do
     end
 
     it 'should perform default configuration of' do
-      midonet_uri = "http://" + params[:midonet_api_ip] + ":" + params[:midonet_api_port] + "/midonet-api";
+      midonet_uri = "http://" + params[:midonet_cluster_ip] + ":" + params[:midonet_cluster_port] + "/midonet-api";
       is_expected.to contain_neutron_plugin_midonet('MIDONET/midonet_uri').with_value(midonet_uri)
       is_expected.to contain_neutron_plugin_midonet('MIDONET/username').with_value(params[:keystone_username])
       is_expected.to contain_neutron_plugin_midonet('MIDONET/password').with_value(params[:keystone_password])
       is_expected.to contain_neutron_plugin_midonet('MIDONET/project_id').with_value(params[:keystone_tenant])
     end
 
+  end
+
+  shared_examples_for 'neutron midonet plugin using deprecated params' do
+    let :params do
+      {
+        :midonet_api_ip    => '192.168.0.1',
+        :midonet_api_port  => '8181',
+      }
+    end
+    before do
+      params.merge!(default_params)
+    end
+    it 'should take into account deprecated parameters first' do
+      midonet_uri = "http://" + params[:midonet_api_ip] + ":" + params[:midonet_api_port] + "/midonet-api";
+      is_expected.to contain_neutron_plugin_midonet('MIDONET/midonet_uri').with_value(midonet_uri)
+    end
+    it 'should take into account deprecated parameters first' do
+      bad_midonet_uri = "http://" + params[:midonet_cluster_ip] + ":" + params[:midonet_cluster_port] + "/midonet-api";
+      is_expected.to_not contain_neutron_plugin_midonet('MIDONET/midonet_uri').with_value(bad_midonet_uri)
+    end
   end
 
   context 'on Debian platforms' do
@@ -82,6 +102,7 @@ describe 'neutron::plugins::midonet' do
       is_expected.to contain_file_line('/etc/default/neutron-server:NEUTRON_PLUGIN_CONFIG').that_notifies('Anchor[neutron::config::end]')
     end
     it_configures 'neutron midonet plugin'
+    it_configures 'neutron midonet plugin using deprecated params'
   end
 
   context 'on RedHat platforms' do
@@ -92,6 +113,7 @@ describe 'neutron::plugins::midonet' do
       }))
     end
     it_configures 'neutron midonet plugin'
+    it_configures 'neutron midonet plugin using deprecated params'
   end
 
 end

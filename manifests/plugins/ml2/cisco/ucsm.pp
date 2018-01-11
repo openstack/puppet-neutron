6a0f77224c3a26d393fbca341f3ee873071bd773
Sandhya Dasu <sadasu@cisco.com>
2018-01-11 16:17:41 -0500

    New configuration being added for Cisco UCSM ML2 driver
    Cisco UCSM ML2 driver now accepts the following new configuration
    parameters:
    1. ucsm_https_verify
    2. sp_template_list
    3. vnic_template_list
    
     Conflicts:
           manifests/plugins/ml2/cisco/ucsm.pp
           spec/classes/neutron_plugins_ml2_cisco_ucsm_spec.rb
    
    Change-Id: I974c51a4bee4dc2e084cea809b3f93978d09c63b
    Closes-Bug: #1742796
    (cherry picked from commit b61e1f5e337fcd9fb198ee8568157d28cf1d8bf6)

--- manifests/plugins/ml2/cisco/ucsm.pp
+++ manifests/plugins/ml2/cisco/ucsm.pp
@@ -1,7 +1,7 @@
 #
 # Configure the Mech Driver for Cisco UCSM plugin
 # More info available here:
-# http://docwiki.cisco.com/wiki/UCS_Mechanism_Driver_for_ML2_Plugin:_Kilo
+# http://networking-cisco.readthedocs.io
 #
 # === Parameters
 #
@@ -27,13 +27,34 @@
 #   Example:
 #   [ '2222:3333', '4444:5555' ]
 #
-
+# [*ucsm_https_verify*]
+#   (optional) Set to False to turn off SSL certificate checking
+#   while connecting to UCS Manager.
+#   Defaults to True.
+#
+# [*sp_template_list*]
+#   (optional) This configuration needs to be provided when
+#   UCS Servers are controlled by Service Profile Templates.
+#   Example:
+#   sp_template_list = SP_Template1_path:SP_Template1:S1,S2
+#                      SP_Template2_path:SP_Template2:S3,S4,S5
+#
+# [*vnic_template_list*]
+#   (optional) This configuration needs to be provided when vNICs
+#   on UCS Servers are controlled by vNIC Templates.
+#   Example:
+#   vnic_template_list = physnet1:vnic_template_path1:vt1
+#                        physnet2:vnic_template_path2:vt2
+#
 class neutron::plugins::ml2::cisco::ucsm (
   $ucsm_ip,
   $ucsm_username,
   $ucsm_password,
   $ucsm_host_list,
+  $sp_template_list,
+  $vnic_template_list,
   $supported_pci_devs = $::os_service_default,
+  $ucsm_https_verify = $::os_service_default,
 ) {
 
   include ::neutron::deps
@@ -44,7 +65,10 @@ class neutron::plugins::ml2::cisco::ucsm (
     'ml2_cisco_ucsm/ucsm_username'      : value => $ucsm_username;
     'ml2_cisco_ucsm/ucsm_password'      : value => $ucsm_password;
     'ml2_cisco_ucsm/ucsm_host_list'     : value => $ucsm_host_list;
+    'ml2_cisco_ucsm/sp_template_list'   : value => $sp_template_list;
+    'ml2_cisco_ucsm/vnic_template_list' : value => $vnic_template_list;
     'ml2_cisco_ucsm/supported_pci_devs' : value => $supported_pci_devs;
+    'ml2_cisco_ucsm/ucsm_https_verify'  : value => $ucsm_https_verify;
   }
 }
 

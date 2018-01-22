698802597358a8e44091db56d59ad931feed8726
Harald Jensas <hjensas@redhat.com>
2018-01-22 10:21:49 +0100

    Add networking-baremetal ml2 plug-in
    Change-Id: Iddbda9741456d66c664bebfb675cd453c6e66b65

---
+++ manifests/plugins/ml2/networking_baremetal.pp
@@ -0,0 +1,31 @@
+# This class installs and configures the networking-baremetal Neutron plugin
+#
+# == Class: neutron::plugins::ml2::networking_baremetal
+#
+# === Parameters
+#
+# [*package_ensure*]
+#   (optional) The intended state of the python-networking-baremetal
+#   package, i.e. any of the possible values of the 'ensure'
+#   property for a package resource type.
+#   Defaults to 'present'
+#
+class neutron::plugins::ml2::networking_baremetal(
+  $package_ensure = 'present',
+  ) {
+
+  include ::neutron::deps
+  include ::neutron::params
+  require ::neutron::plugins::ml2
+
+  if($::osfamily != 'RedHat') {
+    # Drivers are only packaged for RedHat at this time
+    fail("Unsupported osfamily ${::osfamily}")
+  } else {
+    package { 'python2-networking-baremetal':
+      ensure => $package_ensure,
+      name   => $::neutron::params::networking_baremetal_package,
+      tag    => ['openstack', 'neutron-package'],
+    }
+  }
+}

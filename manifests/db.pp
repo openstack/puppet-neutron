f6c7f2d1dd4dbbd5da2afcdc38a0cadb2cd04e73
ZhongShengping <chdzsp@163.com>
2018-03-22 11:09:50 +0800

    Add pool_timeout option
    Add pool_timeout option to configure this value for pool_timeout with
    SQLAlchemy.
    
    Change-Id: Ia34de55c60b10a2aff91c0856333a00a44fa5a81
    Closes-Bug: #1757581

--- manifests/db.pp
+++ manifests/db.pp
@@ -33,6 +33,10 @@
 #   If set, use this value for max_overflow with sqlalchemy.
 #   (Optional) Defaults to $::os_service_default.
 #
+# [*database_pool_timeout*]
+#   (Optional) If set, use this value for pool_timeout with SQLAlchemy.
+#   Defaults to $::os_service_default
+#
 # [*database_db_max_retries*]
 #   (Optional) Maximum retries in case of connection error or deadlock error
 #   before error is raised. Set to -1 to specify an infinite retry count.
@@ -46,6 +50,7 @@ class neutron::db (
   $database_max_retries    = $::os_service_default,
   $database_retry_interval = $::os_service_default,
   $database_max_overflow   = $::os_service_default,
+  $database_pool_timeout   = $::os_service_default,
   $database_db_max_retries = $::os_service_default,
 ) {
 
@@ -74,6 +79,7 @@ class neutron::db (
       retry_interval => $database_retry_interval_real,
       max_pool_size  => $database_max_pool_size_real,
       max_overflow   => $database_max_overflow_real,
+      pool_timeout   => $database_pool_timeout,
       db_max_retries => $database_db_max_retries,
     }
 

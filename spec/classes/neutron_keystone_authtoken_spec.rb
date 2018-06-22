38285dc8792da30a97b5d5bfa17c687d51270ca8
Iago Santos Pardo <iago.santos.pardo@cern.ch>
2018-06-22 11:36:52 +0200

    Service_token_roles_required missing in the server config file
    Service_token_roles_required missing in the server config file which
    allows backwards compatibility to ensure that the service tokens are
    compared against a list of possible roles for validity.
    
    Change-Id: Ib26a4888425fe5dbaf4beca9ef0b7a2dad887551
    Implements: bug service_token_roles_required missing
    Closes-Bug: 1778198

--- spec/classes/neutron_keystone_authtoken_spec.rb
+++ spec/classes/neutron_keystone_authtoken_spec.rb
@@ -44,6 +44,7 @@ describe 'neutron::keystone::authtoken' do
         is_expected.to contain_neutron_config('keystone_authtoken/memcached_servers').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_neutron_config('keystone_authtoken/region_name').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_neutron_config('keystone_authtoken/token_cache_time').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_neutron_config('keystone_authtoken/service_token_roles_required').with_value('<SERVICE DEFAULT>')
       end
     end
 
@@ -84,6 +85,7 @@ describe 'neutron::keystone::authtoken' do
           :manage_memcache_package              => true,
           :region_name                          => 'region2',
           :token_cache_time                     => '301',
+          :service_token_roles_required         => false,
         })
       end
 
@@ -121,6 +123,7 @@ describe 'neutron::keystone::authtoken' do
         is_expected.to contain_neutron_config('keystone_authtoken/memcached_servers').with_value('memcached01:11211,memcached02:11211')
         is_expected.to contain_neutron_config('keystone_authtoken/region_name').with_value(params[:region_name])
         is_expected.to contain_neutron_config('keystone_authtoken/token_cache_time').with_value(params[:token_cache_time])
+        is_expected.to contain_neutron_config('keystone_authtoken/service_token_roles_required').with_value(params[:service_token_roles_required])
       end
 
       it 'installs python memcache package' do

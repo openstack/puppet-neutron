7de1fd602f2d3390c6df58dc6278feffa39e32ff
ZhongShengping <chdzsp@163.com>
2019-04-28 15:00:11 +0800

    Deprecate idle_timeout option
    The idle_timeout parameter is deprecated, use connection_recycle_time
    instead[1].
    
    [1]https://review.opendev.org/#/c/334182/
    
    Change-Id: I9813fedbdeabdc0cda1e29ffe97214dec7795c94
    Depends-On: https://review.opendev.org/656106/
    Closes-Bug: #1826692

--- manifests/db.pp
+++ manifests/db.pp
@@ -8,7 +8,7 @@
 #   Url used to connect to database.
 #   (Optional) Defaults to 'sqlite:////var/lib/neutron/ovs.sqlite'.
 #
-# [*database_idle_timeout*]
+# [*database_connection_recycle_time*]
 #   Timeout when db connections should be reaped.
 #   (Optional) Defaults to $::os_service_default.
 #
@@ -42,24 +42,38 @@
 #   before error is raised. Set to -1 to specify an infinite retry count.
 #   Defaults to $::os_service_default
 #
+# DEPRECATED PARAMETERS
+#
+# [*database_idle_timeout*]
+#   Timeout when db connections should be reaped.
+#   Defaults to undef.
+#
 class neutron::db (
-  $database_connection     = 'sqlite:////var/lib/neutron/ovs.sqlite',
-  $database_idle_timeout   = $::os_service_default,
-  $database_min_pool_size  = $::os_service_default,
-  $database_max_pool_size  = $::os_service_default,
-  $database_max_retries    = $::os_service_default,
-  $database_retry_interval = $::os_service_default,
-  $database_max_overflow   = $::os_service_default,
-  $database_pool_timeout   = $::os_service_default,
-  $database_db_max_retries = $::os_service_default,
+  $database_connection              = 'sqlite:////var/lib/neutron/ovs.sqlite',
+  $database_connection_recycle_time = $::os_service_default,
+  $database_min_pool_size           = $::os_service_default,
+  $database_max_pool_size           = $::os_service_default,
+  $database_max_retries             = $::os_service_default,
+  $database_retry_interval          = $::os_service_default,
+  $database_max_overflow            = $::os_service_default,
+  $database_pool_timeout            = $::os_service_default,
+  $database_db_max_retries          = $::os_service_default,
+  # DEPRECATED PARAMETERS
+  $database_idle_timeout            = undef,
 ) {
 
   include ::neutron::deps
 
+  if $database_idle_timeout {
+    warning('The database_idle_timeout parameter is deprecated. Please use \
+database_connection_recycle_time instead.')
+  }
+
   # NOTE(spredzy): In order to keep backward compatibility we rely on the pick function
   # to use neutron::<myparam> if neutron::db::<myparam> isn't specified.
   $database_connection_real = pick($::neutron::server::database_connection, $database_connection)
-  $database_idle_timeout_real = pick($::neutron::server::database_idle_timeout, $database_idle_timeout)
+  $database_connection_recycle_time_real = pick($::neutron::server::database_idle_timeout, $database_idle_timeout,
+                                                $database_connection_recycle_time)
   $database_min_pool_size_real = pick($::neutron::server::database_min_pool_size, $database_min_pool_size)
   $database_max_pool_size_real = pick($::neutron::server::database_max_pool_size, $database_max_pool_size)
   $database_max_retries_real = pick($::neutron::server::database_max_retries, $database_max_retries)
@@ -72,15 +86,15 @@ class neutron::db (
   if $database_connection_real {
 
     oslo::db { 'neutron_config':
-      connection     => $database_connection_real,
-      idle_timeout   => $database_idle_timeout_real,
-      min_pool_size  => $database_min_pool_size_real,
-      max_retries    => $database_max_retries_real,
-      retry_interval => $database_retry_interval_real,
-      max_pool_size  => $database_max_pool_size_real,
-      max_overflow   => $database_max_overflow_real,
-      pool_timeout   => $database_pool_timeout,
-      db_max_retries => $database_db_max_retries,
+      connection              => $database_connection_real,
+      connection_recycle_time => $database_connection_recycle_time_real,
+      min_pool_size           => $database_min_pool_size_real,
+      max_retries             => $database_max_retries_real,
+      retry_interval          => $database_retry_interval_real,
+      max_pool_size           => $database_max_pool_size_real,
+      max_overflow            => $database_max_overflow_real,
+      pool_timeout            => $database_pool_timeout,
+      db_max_retries          => $database_db_max_retries,
     }
 
   }
--- spec/classes/neutron_db_spec.rb
+++ spec/classes/neutron_db_spec.rb
@@ -4,43 +4,43 @@ describe 'neutron::db' do
   shared_examples 'neutron::db' do
     context 'with default parameters' do
       it { should contain_oslo__db('neutron_config').with(
-        :db_max_retries => '<SERVICE DEFAULT>',
-        :connection     => 'sqlite:////var/lib/neutron/ovs.sqlite',
-        :idle_timeout   => '<SERVICE DEFAULT>',
-        :min_pool_size  => '<SERVICE DEFAULT>',
-        :max_pool_size  => '<SERVICE DEFAULT>',
-        :max_retries    => '<SERVICE DEFAULT>',
-        :retry_interval => '<SERVICE DEFAULT>',
-        :max_overflow   => '<SERVICE DEFAULT>',
-        :pool_timeout   => '<SERVICE DEFAULT>',
+        :db_max_retries          => '<SERVICE DEFAULT>',
+        :connection              => 'sqlite:////var/lib/neutron/ovs.sqlite',
+        :connection_recycle_time => '<SERVICE DEFAULT>',
+        :min_pool_size           => '<SERVICE DEFAULT>',
+        :max_pool_size           => '<SERVICE DEFAULT>',
+        :max_retries             => '<SERVICE DEFAULT>',
+        :retry_interval          => '<SERVICE DEFAULT>',
+        :max_overflow            => '<SERVICE DEFAULT>',
+        :pool_timeout            => '<SERVICE DEFAULT>',
       )}
     end
 
     context 'with specific parameters' do
       let :params do
         {
-          :database_connection     => 'mysql+pymysql://neutron:neutron@localhost/neutron',
-          :database_idle_timeout   => '3601',
-          :database_min_pool_size  => '2',
-          :database_max_pool_size  => '11',
-          :database_max_retries    => '11',
-          :database_retry_interval => '11',
-          :database_db_max_retries => '-1',
-          :database_max_overflow   => '21',
-          :database_pool_timeout   => '21',
+          :database_connection              => 'mysql+pymysql://neutron:neutron@localhost/neutron',
+          :database_connection_recycle_time => '3601',
+          :database_min_pool_size           => '2',
+          :database_max_pool_size           => '11',
+          :database_max_retries             => '11',
+          :database_retry_interval          => '11',
+          :database_db_max_retries          => '-1',
+          :database_max_overflow            => '21',
+          :database_pool_timeout            => '21',
         }
       end
 
       it { should contain_oslo__db('neutron_config').with(
-        :db_max_retries => '-1',
-        :connection     => 'mysql+pymysql://neutron:neutron@localhost/neutron',
-        :idle_timeout   => '3601',
-        :min_pool_size  => '2',
-        :max_pool_size  => '11',
-        :max_retries    => '11',
-        :retry_interval => '11',
-        :max_overflow   => '21',
-        :pool_timeout   => '21',
+        :db_max_retries          => '-1',
+        :connection              => 'mysql+pymysql://neutron:neutron@localhost/neutron',
+        :connection_recycle_time => '3601',
+        :min_pool_size           => '2',
+        :max_pool_size           => '11',
+        :max_retries             => '11',
+        :retry_interval          => '11',
+        :max_overflow            => '21',
+        :pool_timeout            => '21',
       )}
     end
   end

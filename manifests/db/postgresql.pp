7244048a70e62ded0a24db0694123f79348e27a5
Takashi Kajinami <tkajinam@redhat.com>
2020-05-19 20:27:12 +0900

    Remove password hash generation in each puppet modules
    ... and migrate it to openstacklib so that all logics about database
    configuration are implemented in one common place.
    
    Depends-on: https://review.opendev.org/#/c/728595/
    Change-Id: I7a1ef6c25871b993279413c6bbfbee38838eb4d9

--- manifests/db/postgresql.pp
+++ manifests/db/postgresql.pp
@@ -35,11 +35,11 @@ class neutron::db::postgresql(
   include neutron::deps
 
   ::openstacklib::db::postgresql { 'neutron':
-    password_hash => postgresql_password($user, $password),
-    dbname        => $dbname,
-    user          => $user,
-    encoding      => $encoding,
-    privileges    => $privileges,
+    password   => $password,
+    dbname     => $dbname,
+    user       => $user,
+    encoding   => $encoding,
+    privileges => $privileges,
   }
 
   Anchor['neutron::db::begin']

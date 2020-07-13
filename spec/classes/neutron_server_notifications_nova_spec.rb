4638137d449082cd95931bd792aa631ff39ca78f
Takashi Kajinami <tkajinam@redhat.com>
2020-07-13 15:25:52 +0900

    Create an independent class for nova notifications
    This patch introcues a new class, neutron::server::notifications::nova,
    to manage all configuration parameters for nova notifications.
    
    Note that the default values for notify_nova_on_port_status_changes and
    notify_nova_on_data_status_changes are set to $::os_service_default in
    this change, to use the default values defined in neutron (True)
    instead of overriding it in puppet-neutron.
    
    Change-Id: Ife7d196883f34aa53e7d6311bdcdb286e8ba275c

---
+++ spec/classes/neutron_server_notifications_nova_spec.rb
@@ -0,0 +1,87 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Unit tests for neutron::server::notifications::nova class
+#
+
+require 'spec_helper'
+
+describe 'neutron::server::notifications::nova' do
+  let :params do
+    {
+      :auth_type                          => 'password',
+      :username                           => 'nova',
+      :password                           => 'secrete',
+      :project_domain_name                => 'Default',
+      :project_name                       => 'services',
+      :user_domain_name                   => 'Default',
+      :auth_url                           => 'http://127.0.0.1:5000',
+    }
+  end
+
+  shared_examples 'neutron::server::notifications::nova' do
+    it 'configure neutron.conf' do
+      should contain_neutron_config('DEFAULT/notify_nova_on_port_status_changes').with_value('<SERVICE DEFAULT>')
+      should contain_neutron_config('DEFAULT/notify_nova_on_port_data_changes').with_value('<SERVICE DEFAULT>')
+      should contain_neutron_config('nova/auth_url').with_value('http://127.0.0.1:5000')
+      should contain_neutron_config('nova/auth_type').with_value('password')
+      should contain_neutron_config('nova/username').with_value('nova')
+      should contain_neutron_config('nova/password').with_value('secrete').with_secret( true )
+      should contain_neutron_config('nova/region_name').with_value('<SERVICE DEFAULT>')
+      should contain_neutron_config('nova/project_domain_name').with_value('Default')
+      should contain_neutron_config('nova/user_domain_name').with_value('Default')
+      should contain_neutron_config('nova/endpoint_type').with_value('<SERVICE DEFAULT>')
+    end
+
+    context 'when overriding parameters' do
+      before :each do
+        params.merge!(
+          :notify_nova_on_port_status_changes => false,
+          :notify_nova_on_port_data_changes   => false,
+          :auth_url                           => 'http://keystone:5000/v3',
+          :auth_type                          => 'password',
+          :username                           => 'joe',
+          :region_name                        => 'MyRegion',
+          :project_domain_name                => 'Default_1',
+          :user_domain_name                   => 'Default_2',
+          :endpoint_type                      => 'internal',
+        )
+      end
+
+      it 'should configure neutron server with overrided parameters' do
+        should contain_neutron_config('DEFAULT/notify_nova_on_port_status_changes').with_value(false)
+        should contain_neutron_config('DEFAULT/notify_nova_on_port_data_changes').with_value(false)
+        should contain_neutron_config('nova/auth_url').with_value('http://keystone:5000/v3')
+        should contain_neutron_config('nova/auth_type').with_value('password')
+        should contain_neutron_config('nova/username').with_value('joe')
+        should contain_neutron_config('nova/password').with_value('secrete').with_secret( true )
+        should contain_neutron_config('nova/region_name').with_value('MyRegion')
+        should contain_neutron_config('nova/project_domain_name').with_value('Default_1')
+        should contain_neutron_config('nova/user_domain_name').with_value('Default_2')
+        should contain_neutron_config('nova/endpoint_type').with_value('internal')
+      end
+    end
+
+  end
+
+  on_supported_os({
+    :supported_os => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      it_behaves_like 'neutron::server::notifications::nova'
+    end
+  end
+end

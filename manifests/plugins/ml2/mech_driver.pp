28ca6097fcdb92998d0e14d8f2b110e8263beb13
Takashi Kajinami <tkajinam@redhat.com>
2020-10-10 13:02:13 +0900

    Deprecate duplicated parameters for vnic_type_blacklist
    ... because now the vnic_type_blacklist parameters are implemeted in
    ovs_driver and sriov_driver class.
    
    Change-Id: Iaa52568d7a1c8bde97a24e87ead863c46a28b408

--- manifests/plugins/ml2/mech_driver.pp
+++ manifests/plugins/ml2/mech_driver.pp
@@ -23,6 +23,8 @@
 #   (required) Supported PCI vendor devices, defined by vendor_id:product_id according
 #   to the PCI ID Repository. Default enables support for Intel and Mellanox SR-IOV capable NICs
 #
+# DEPRECATED PARAMETERS
+#
 # [*ovs_vnic_type_blacklist*]
 #  (optional) list of VNIC types for which support in Neutron is
 #  administratively prohibited by the OVS mechanism driver
@@ -33,8 +35,9 @@
 #
 define neutron::plugins::ml2::mech_driver (
   $supported_pci_vendor_devs,
-  $ovs_vnic_type_blacklist      = [],
-  $sriov_vnic_type_blacklist    = [],
+  # DEPRECATED PARAMETERS
+  $ovs_vnic_type_blacklist      = undef,
+  $sriov_vnic_type_blacklist    = undef,
 ){
 
   include neutron::deps
@@ -62,15 +65,19 @@ define neutron::plugins::ml2::mech_driver (
     }
   }
 
-  if ($ovs_vnic_type_blacklist != []) {
-    neutron_plugin_ml2 {
-      'ovs_driver/vnic_type_blacklist': value => join(any2array($ovs_vnic_type_blacklist), ',');
+  if ($ovs_vnic_type_blacklist != undef) {
+    warning('neutron::plugins::ml2::mech_driver::ovs_vnic_type_blacklist has been \
+deperecated. Use neutron::plugins::ml2::ovs_driver class')
+    class { 'neutron::plugins::ml2::ovs_driver':
+      vnic_type_blacklist => $ovs_vnic_type_blacklist
     }
   }
 
-  if ($sriov_vnic_type_blacklist != []) {
-    neutron_plugin_ml2 {
-      'sriov_driver/vnic_type_blacklist': value => join(any2array($sriov_vnic_type_blacklist), ',');
+  if ($sriov_vnic_type_blacklist != undef) {
+    warning('neutron::plugins::ml2::mech_driver::sriov_vnic_type_blacklist has been \
+deperecated. Use neutron::plugins::ml2::sriov_driver class')
+    class { 'neutron::plugins::ml2::sriov_vnic_type_blacklist':
+      vnic_type_blacklist => $sriov_vnic_type_blacklist
     }
   }
 }

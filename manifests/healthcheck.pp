66f78837b184777e43247c3fd39a758686b768b4
Takashi Kajinami <tkajinam@redhat.com>
2021-04-21 08:09:00 +0900

    Add support for healthcheck middleware options
    Depends-on: https://review.opendev.org/772202
    Change-Id: Ie0e7ddef6f2c2e1e52cc8f6085daaa4805b013d4
    (cherry picked from commit 3b93693c41577e951eacb327c8d05f9594c49abe)

---
+++ manifests/healthcheck.pp
@@ -0,0 +1,41 @@
+# == Class: neutron::healthcheck
+#
+# Configure oslo_middleware options in healthcheck section
+#
+# == Params
+#
+# [*detailed*]
+#   (Optional) Show more detailed information as part of the response.
+#   Defaults to $::os_service_default
+#
+# [*backends*]
+#   (Optional) Additional backends that can perform health checks and report
+#   that information back as part of a request.
+#   Defaults to $::os_service_default
+#
+# [*disable_by_file_path*]
+#   (Optional) Check the presense of a file to determine if an application
+#   is running on a port.
+#   Defaults to $::os_service_default
+#
+# [*disable_by_file_paths*]
+#   (Optional) Check the presense of a file to determine if an application
+#   is running on a port. Expects a "port:path" list of strings.
+#   Defaults to $::os_service_default
+#
+class neutron::healthcheck (
+  $detailed              = $::os_service_default,
+  $backends              = $::os_service_default,
+  $disable_by_file_path  = $::os_service_default,
+  $disable_by_file_paths = $::os_service_default,
+) {
+
+  include neutron::deps
+
+  oslo::healthcheck { 'neutron_config':
+    detailed              => $detailed,
+    backends              => $backends,
+    disable_by_file_path  => $disable_by_file_path,
+    disable_by_file_paths => $disable_by_file_paths,
+  }
+}

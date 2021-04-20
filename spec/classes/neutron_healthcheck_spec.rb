3b93693c41577e951eacb327c8d05f9594c49abe
Takashi Kajinami <tkajinam@redhat.com>
2021-04-21 08:09:00 +0900

    Add support for healthcheck middleware options
    Depends-on: https://review.opendev.org/772202
    Change-Id: Ie0e7ddef6f2c2e1e52cc8f6085daaa4805b013d4

---
+++ spec/classes/neutron_healthcheck_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe 'neutron::healthcheck' do
+
+  shared_examples_for 'neutron::healthcheck' do
+
+    context 'with default parameters' do
+      let :params do
+        {}
+      end
+
+      it 'configures default values' do
+        is_expected.to contain_oslo__healthcheck('neutron_config').with(
+          :detailed              => '<SERVICE DEFAULT>',
+          :backends              => '<SERVICE DEFAULT>',
+          :disable_by_file_path  => '<SERVICE DEFAULT>',
+          :disable_by_file_paths => '<SERVICE DEFAULT>',
+        )
+      end
+    end
+
+    context 'with specific parameters' do
+      let :params do
+        {
+          :detailed              => true,
+          :backends              => ['disable_by_file'],
+          :disable_by_file_path  => '/etc/neutron/healthcheck/disabled',
+          :disable_by_file_paths => ['9696:/etc/neutron/healthcheck/disabled'],
+        }
+      end
+
+      it 'configures specified values' do
+        is_expected.to contain_oslo__healthcheck('neutron_config').with(
+          :detailed              => true,
+          :backends              => ['disable_by_file'],
+          :disable_by_file_path  => '/etc/neutron/healthcheck/disabled',
+          :disable_by_file_paths => ['9696:/etc/neutron/healthcheck/disabled'],
+        )
+      end
+    end
+  end
+
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      it_configures 'neutron::healthcheck'
+    end
+  end
+
+end

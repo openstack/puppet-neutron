327bc154d5955de3150d5c51c31aab1047f84755
Takashi Kajinami <tkajinam@redhat.com>
2021-05-08 14:34:42 +0900

    Remove XenAPI parameters from neutron::rootwrap
    ... because these parameter were deprecated during the previous cycle.
    
    Change-Id: I54d5761c4111d2f47efec7e185390a8dd06a6ea3

--- manifests/rootwrap.pp
+++ manifests/rootwrap.pp
@@ -4,42 +4,8 @@
 #
 # === Parameters:
 #
-# DEPRECATED PARAMETERS
-#
-# [*xenapi_connection_url*]
-#   (optional) XenAPI connection URL. Only needed when target a XenServer/XCP
-#   compute host's dom0
-#   Defaults to undef.
-#
-# [*xenapi_connection_username*]
-#   (optional) XenAPI username. Only needed when target a XenServer/XCP
-#   compute host's dom0
-#   Defaults to undef.
-#
-# [*xenapi_connection_password*]
-#   (optional) XenAPI connection password. Only needed when target a XenServer/XCP
-#   compute host's dom0
-#   Defaults to undef.
-#
 class neutron::rootwrap (
-  # DEPRECATED PARAMETERS
-  $xenapi_connection_url      = undef,
-  $xenapi_connection_username = undef,
-  $xenapi_connection_password = undef,
 ) {
 
   include neutron::deps
-
-  $deprecated_xenapi_param_names = [
-    'xenapi_connection_url',
-    'xenapi_connection_username',
-    'xenapi_connection_password',
-  ]
-  $deprecated_xenapi_param_names.each |$param_name| {
-    $param = getvar($param_name)
-    if $param != undef {
-      warning("The ${param_name} parameter is deprecated and has no effect.")
-    }
-  }
-
 }

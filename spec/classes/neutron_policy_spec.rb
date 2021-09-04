b276901eed0594efb8845a3340b5aa33dc87053b
Takashi Kajinami <tkajinam@redhat.com>
2021-09-04 22:15:41 +0900

    Allow purging policy files
    This change introduces the new purge_config parameter to the policy
    class so that any policy rules not managed by puppet manifests can be
    cleared.
    
    Co-Authored-By: Martin Schuppert <mschuppert@redhat.com>
    Depends-On: https://review.opendev.org/802305
    Change-Id: Ief0ba5da927484a4e691a1e1796b1e9e4879d8df

--- spec/classes/neutron_policy_spec.rb
+++ spec/classes/neutron_policy_spec.rb
@@ -2,35 +2,72 @@ require 'spec_helper'
 
 describe 'neutron::policy' do
   shared_examples 'neutron::policy' do
-    let :params do
-      {
-        :enforce_scope        => false,
-        :enforce_new_defaults => false,
-        :policy_path          => '/etc/neutron/policy.yaml',
-        :policy_dirs          => '/etc/neutron/policy.d',
-        :policies             => {
-          'context_is_admin' => {
-            'key'   => 'context_is_admin',
-            'value' => 'foo:bar'
+
+    context 'setup policy with parameters' do
+      let :params do
+        {
+          :enforce_scope        => false,
+          :enforce_new_defaults => false,
+          :policy_path          => '/etc/neutron/policy.yaml',
+          :policy_dirs          => '/etc/neutron/policy.d',
+          :policies             => {
+            'context_is_admin' => {
+              'key'   => 'context_is_admin',
+              'value' => 'foo:bar'
+            }
           }
         }
-      }
+      end
+
+      it 'set up the policies' do
+        is_expected.to contain_openstacklib__policy('/etc/neutron/policy.yaml').with(
+          :policies     => {
+            'context_is_admin' => {
+              'key'   => 'context_is_admin',
+              'value' => 'foo:bar'
+            }
+          },
+          :policy_path  => '/etc/neutron/policy.yaml',
+          :file_user    => 'root',
+          :file_group   => 'neutron',
+          :file_format  => 'yaml',
+          :purge_config => false,
+        )
+        is_expected.to contain_oslo__policy('neutron_config').with(
+          :enforce_scope        => false,
+          :enforce_new_defaults => false,
+          :policy_file          => '/etc/neutron/policy.yaml',
+          :policy_dirs          => '/etc/neutron/policy.d',
+        )
+      end
     end
 
-    it 'set up the policies' do
-      is_expected.to contain_openstacklib__policy__base('context_is_admin').with({
-        :key         => 'context_is_admin',
-        :value       => 'foo:bar',
-        :file_user   => 'root',
-        :file_group  => 'neutron',
-        :file_format => 'yaml',
-      })
-      is_expected.to contain_oslo__policy('neutron_config').with(
-        :enforce_scope        => false,
-        :enforce_new_defaults => false,
-        :policy_file          => '/etc/neutron/policy.yaml',
-        :policy_dirs          => '/etc/neutron/policy.d',
-      )
+    context 'with empty policies and purge_config enabled' do
+      let :params do
+        {
+          :enforce_scope        => false,
+          :enforce_new_defaults => false,
+          :policy_path          => '/etc/neutron/policy.yaml',
+          :policies             => {},
+          :purge_config         => true,
+        }
+      end
+
+      it 'set up the policies' do
+        is_expected.to contain_openstacklib__policy('/etc/neutron/policy.yaml').with(
+          :policies     => {},
+          :policy_path  => '/etc/neutron/policy.yaml',
+          :file_user    => 'root',
+          :file_group   => 'neutron',
+          :file_format  => 'yaml',
+          :purge_config => true,
+        )
+        is_expected.to contain_oslo__policy('neutron_config').with(
+          :enforce_scope        => false,
+          :enforce_new_defaults => false,
+          :policy_file          => '/etc/neutron/policy.yaml',
+        )
+      end
     end
   end
 

d7fe8f2978df88cb01fd9a5f3a02331f66f62a6f
Takashi Kajinami <tkajinam@redhat.com>
2021-11-09 21:16:12 +0900

    ml2+ovn: Add more tunable parameters for OVN DB connections
    Change-Id: I9a93b65aa5dbf4bca934aec82aad2f7f6ab3d73c
    (cherry picked from commit 85db34491e152ff677c952310206a24270803a84)
    (cherry picked from commit 9d553d51c48c6f9c0ffbf46d409bc8c0590ef4ea)
    (cherry picked from commit 4456238f978d37dbdac8769d3d3db89ed56ef418)

--- spec/classes/neutron_plugins_ml2_ovn_spec.rb
+++ spec/classes/neutron_plugins_ml2_ovn_spec.rb
@@ -19,7 +19,9 @@ describe 'neutron::plugins::ml2::ovn' do
        :ovn_sb_private_key       => 'sb_key',
        :ovn_sb_certificate       => 'sb_cert',
        :ovn_sb_ca_cert           => 'sb_ca_cert',
-       :ovsdb_connection_timeout => '60',
+       :ovsdb_connection_timeout => 60,
+       :ovsdb_retry_max_interval => 180,
+       :ovsdb_probe_interval     => 60000,
        :neutron_sync_mode        => 'log',
        :ovn_l3_mode              => true,
        :vif_type                 => 'ovs',
@@ -49,6 +51,8 @@ describe 'neutron::plugins::ml2::ovn' do
       should contain_neutron_plugin_ml2('ovn/ovn_sb_certificate').with_value(params[:ovn_sb_certificate])
       should contain_neutron_plugin_ml2('ovn/ovn_sb_ca_cert').with_value(params[:ovn_sb_ca_cert])
       should contain_neutron_plugin_ml2('ovn/ovsdb_connection_timeout').with_value(params[:ovsdb_connection_timeout])
+      should contain_neutron_plugin_ml2('ovn/ovsdb_retry_max_interval').with_value(params[:ovsdb_retry_max_interval])
+      should contain_neutron_plugin_ml2('ovn/ovsdb_probe_interval').with_value(params[:ovsdb_probe_interval])
       should contain_neutron_plugin_ml2('ovn/neutron_sync_mode').with_value(params[:neutron_sync_mode])
       should contain_neutron_plugin_ml2('ovn/ovn_l3_mode').with_value(params[:ovn_l3_mode])
       should contain_neutron_plugin_ml2('ovn/vif_type').with_value(params[:vif_type])

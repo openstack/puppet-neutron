openstack/puppet-neutron

commit dfeccd674988f34edea5ca7b95bdeea827bab000

Deprecate domain_id parameters
Currently we set both domain_name and domain_id for some configuration
files, but it makes it difficult for operators to use their original
domain because they should update both parameters correctly.

Because it's redundant to required both name and id set, this patch
deprecates all domain_id parameters, so that operator needs to set
their domain name, which is predictable, before creating actual domain.

Change-Id: Id96ab9af7ac5776d5a0000889d26e494c68d9376

manifests/agents/ml2/networking_baremetal.pp
@@ -60,10 +60,6 @@
 #   (optional) Username for connection to ironic in admin context
 #   Defaults to 'ironic'
 #
-# [*project_domain_id*]
-#   (optional) Domain ID containing project
-#   Defaults to 'default'
-#
 # [*project_domain_name*]
 #   (Optional) Domain name containing project
 #   Defaults to 'Default'
@@ -71,10 +67,6 @@
 # [*project_name*]
 #   (optional) Project name to scope to
 #   Defaults to 'services'
-#
-# [*user_domain_id*]
-#   (optional) User's domain ID for connection to ironic in admin context
-#   Defaults to 'default'
 #
 # [*user_domain_name*]
 #   (Optional) Name of domain for $username
@@ -98,6 +90,16 @@
 #   ironic-neutron-agent config.
 #   Defaults to false.
 #
+# DEPRECATED PARAMETERS
+#
+# [*project_domain_id*]
+#   (optional) Domain ID containing project
+#   Defaults to undef
+#
+# [*user_domain_id*]
+#   (optional) User's domain ID for connection to ironic in admin context
+#   Defaults to undef
+#
 class neutron::agents::ml2::networking_baremetal (
   $password,
   $enabled             = true,
@@ -112,15 +114,16 @@ class neutron::agents::ml2::networking_baremetal (
   $auth_type           = 'password',
   $auth_url            = 'http://127.0.0.1:5000',
   $username            = 'ironic',
-  $project_domain_id   = 'default',
   $project_domain_name = 'Default',
   $project_name        = 'services',
-  $user_domain_id      = 'default',
   $user_domain_name    = 'Default',
   $region_name         = $::os_service_default,
   $retry_interval      = $::os_service_default,
   $max_retries         = $::os_service_default,
   $purge_config        = false,
+  # DEPRECATED PARAMETERS
+  $project_domain_id   = undef,
+  $user_domain_id      = undef,
 ) {
 
   include neutron::deps
@@ -146,14 +149,28 @@ class neutron::agents::ml2::networking_baremetal (
     'ironic/auth_url':            value => $auth_url;
     'ironic/username':            value => $username;
     'ironic/password':            value => $password;
-    'ironic/project_domain_id':   value => $project_domain_id;
     'ironic/project_domain_name': value => $project_domain_name;
     'ironic/project_name':        value => $project_name;
-    'ironic/user_domain_id':      value => $user_domain_id;
     'ironic/user_domain_name':    value => $user_domain_name;
     'ironic/region_name':         value => $region_name;
     'ironic/retry_interval':      value => $retry_interval;
     'ironic/max_retries':         value => $max_retries;
+  }
+
+  if $project_domain_id != undef {
+    warning('project_domain_id is deprecated and will be removed in a future release. \
+Use project_domain_name instead')
+    ironic_neutron_agent_config {
+      'ironic/project_domain_id': value => $project_domain_id;
+    }
+  }
+
+  if $user_domain_id != undef {
+    warning('user_domain_id is deprecated and will be removed in a future release. \
+Use user_domain_name instead')
+    ironic_neutron_agent_config {
+      'ironic/user_domain_id': value => $user_domain_id;
+    }
   }
 
   package { 'python2-ironic-neutron-agent':

spec/classes/neutron_agents_ml2_networking_baremetal_spec.rb
@@ -9,10 +9,8 @@ describe 'neutron::agents::ml2::networking_baremetal' do
       :auth_type           => 'password',
       :auth_url            => 'http://127.0.0.1:5000',
       :username            => 'ironic',
-      :project_domain_id   => 'default',
       :project_domain_name => 'Default',
       :project_name        => 'services',
-      :user_domain_id      => 'default',
       :user_domain_name    => 'Default',
       :purge_config        => false,
     }
@@ -47,10 +45,8 @@ describe 'neutron::agents::ml2::networking_baremetal' do
       should contain_ironic_neutron_agent_config('ironic/auth_url').with_value(p[:auth_url])
       should contain_ironic_neutron_agent_config('ironic/username').with_value(p[:username])
       should contain_ironic_neutron_agent_config('ironic/password').with_value(p[:password])
-      should contain_ironic_neutron_agent_config('ironic/project_domain_id').with_value(p[:project_domain_id])
       should contain_ironic_neutron_agent_config('ironic/project_domain_name').with_value(p[:project_domain_name])
       should contain_ironic_neutron_agent_config('ironic/project_name').with_value(p[:project_name])
-      should contain_ironic_neutron_agent_config('ironic/user_domain_id').with_value(p[:user_domain_id])
       should contain_ironic_neutron_agent_config('ironic/user_domain_name').with_value(p[:user_domain_name])
       should contain_ironic_neutron_agent_config('ironic/region_name').with_value('<SERVICE DEFAULT>')
       should contain_ironic_neutron_agent_config('ironic/retry_interval').with_value('<SERVICE DEFAULT>')

manifests/designate.pp
@@ -27,17 +27,9 @@
 #   (optional) The name of the admin project
 #   Defaults to 'services'
 #
-# [*project_domain_id*]
-#   (optional) Nova project's domain ID
-#   Defaults to 'default'
-#
 # [*project_domain_name*]
 #   (Optional) Name of domain for $project_name
 #   Defaults to 'Default'
-#
-# [*user_domain_id*]
-#   (optional) User's domain ID for connection to designate in admin context
-#   Defaults to 'default'
 #
 # [*user_domain_name*]
 #   (Optional) Name of domain for $username
@@ -61,6 +53,16 @@
 # [*ptr_zone_email*]
 #   (optional) The email address to be used when creating PTR zones.
 #
+# DEPRECATED PARAMETERS
+#
+# [*project_domain_id*]
+#   (optional) Nova project's domain ID
+#   Defaults to undef
+#
+# [*user_domain_id*]
+#   (optional) User's domain ID for connection to designate in admin context
+#   Defaults to undef
+#
 class neutron::designate (
   $password,
   $url,
@@ -68,15 +70,16 @@ class neutron::designate (
   $username                  = 'neutron',
   $project_id                = $::os_service_default,
   $project_name              = 'services',
-  $project_domain_id         = 'default',
   $project_domain_name       = 'Default',
-  $user_domain_id            = 'default',
   $user_domain_name          = 'Default',
   $auth_url                  = 'http://127.0.0.1:5000',
   $allow_reverse_dns_lookup  = $::os_service_default,
   $ipv4_ptr_zone_prefix_size = $::os_service_default,
   $ipv6_ptr_zone_prefix_size = $::os_service_default,
   $ptr_zone_email            = $::os_service_default,
+  # DEPRECATED PARAMETERS
+  $project_domain_id         = undef,
+  $user_domain_id            = undef,
 ) {
   include neutron::deps
   include neutron::params
@@ -89,14 +92,28 @@ class neutron::designate (
     'designate/username':                  value => $username;
     'designate/project_id':                value => $project_id;
     'designate/project_name':              value => $project_name;
-    'designate/project_domain_id':         value => $project_domain_id;
     'designate/project_domain_name':       value => $project_domain_name;
-    'designate/user_domain_id':            value => $user_domain_id;
     'designate/user_domain_name':          value => $user_domain_name;
     'designate/auth_url':                  value => $auth_url;
     'designate/allow_reverse_dns_lookup':  value => $allow_reverse_dns_lookup;
     'designate/ipv4_ptr_zone_prefix_size': value => $ipv4_ptr_zone_prefix_size;
     'designate/ipv6_ptr_zone_prefix_size': value => $ipv6_ptr_zone_prefix_size;
     'designate/ptr_zone_email':            value => $ptr_zone_email;
+  }
+
+  if $project_domain_id != undef {
+    warning('project_domain_id is deprecated and will be removed in a future release. \
+Use project_domain_name instead')
+    neutron_config {
+      'designate/project_domain_id': value => $project_domain_id;
+    }
+  }
+
+  if $user_domain_id != undef {
+    warning('user_domain_id is deprecated and will be removed in a future release. \
+Use user_domain_name instead')
+    neutron_config {
+      'designate/user_domain_id': value => $user_domain_id;
+    }
   }
 }

manifests/server/notifications.pp
@@ -42,10 +42,6 @@
 #   (optional) Username for connection to nova in admin context
 #   Defaults to 'nova'
 #
-# [*project_domain_id*]
-#   (optional) Nova project's domain ID
-#   Defaults to 'default'
-#
 # [*project_domain_name*]
 #   (Optional) Name of domain for $project_name
 #   Defaults to 'Default'
@@ -53,10 +49,6 @@
 # [*project_name*]
 #   (optional) Nova project's name
 #   Defaults to 'services'
-#
-# [*user_domain_id*]
-#   (optional) User's domain ID for connection to nova in admin context
-#   Defaults to 'default'
 #
 # [*user_domain_name*]
 #   (Optional) Name of domain for $username
@@ -89,6 +81,14 @@
 #   (optional) The name of the admin nova tenant
 #   Defaults to undef
 #
+# [*project_domain_id*]
+#   (optional) Nova project's domain ID
+#   Defaults to undef
+#
+# [*user_domain_id*]
+#   (optional) User's domain ID for connection to nova in admin context
+#   Defaults to undef
+#
 class neutron::server::notifications (
   $password,
   $notify_nova_on_port_status_changes = true,
@@ -96,10 +96,8 @@ class neutron::server::notifications (
   $send_events_interval               = $::os_service_default,
   $auth_type                          = 'password',
   $username                           = 'nova',
-  $project_domain_id                  = 'default',
   $project_domain_name                = 'Default',
   $project_name                       = 'services',
-  $user_domain_id                     = 'default',
   $user_domain_name                   = 'Default',
   $auth_url                           = 'http://127.0.0.1:5000',
   $region_name                        = $::os_service_default,
@@ -107,6 +105,8 @@ class neutron::server::notifications (
   # DEPRECATED PARAMETERS
   $tenant_id                          = undef,
   $tenant_name                        = undef,
+  $project_domain_id                  = undef,
+  $user_domain_id                     = undef,
 ) {
 
   include neutron::deps
@@ -125,14 +125,28 @@ has no effect. Use neutron::server::notifications::project_name instead')
     'nova/auth_url':            value => $auth_url;
     'nova/username':            value => $username;
     'nova/password':            value => $password, secret => true;
-    'nova/project_domain_id':   value => $project_domain_id;
     'nova/project_domain_name': value => $project_domain_name;
     'nova/project_name':        value => $project_name;
-    'nova/user_domain_id':      value => $user_domain_id;
     'nova/user_domain_name':    value => $user_domain_name;
     'nova/region_name':         value => $region_name;
     'nova/endpoint_type':       value => $endpoint_type;
     'nova/auth_type':           value => $auth_type;
+  }
+
+  if $project_domain_id != undef {
+    warning('project_domain_id is deprecated and will be removed in a future release. \
+Use project_domain_name instead')
+    neutron_config {
+      'nova/project_domain_id': value => $project_domain_id;
+    }
+  }
+
+  if $user_domain_id != undef {
+    warning('user_domain_id is deprecated and will be removed in a future release. \
+Use user_domain_name instead')
+    neutron_config {
+      'nova/user_domain_id': value => $user_domain_id;
+    }
   }
 
   neutron_config {

manifests/server/placement.pp
@@ -28,10 +28,6 @@
 #   (optional) Username for connection to nova in admin context
 #   Defaults to 'nova'
 #
-# [*project_domain_id*]
-#   (optional) Nova project's domain ID
-#   Defaults to 'default'
-#
 # [*project_domain_name*]
 #   (Optional) Name of domain for $project_name
 #   Defaults to 'Default'
@@ -39,10 +35,6 @@
 # [*project_name*]
 #   (optional) Nova project's name
 #   Defaults to 'services'
-#
-# [*user_domain_id*]
-#   (optional) User's domain ID for connection to nova in admin context
-#   Defaults to 'default'
 #
 # [*user_domain_name*]
 #   (Optional) Name of domain for $username
@@ -64,19 +56,29 @@
 #   the keystone catalog.
 #   Defaults to $::os_service_default
 #
-
+# DEPRECATED PARAMETERS
+#
+# [*project_domain_id*]
+#   (optional) Nova project's domain ID
+#   Defaults to undef
+#
+# [*user_domain_id*]
+#   (optional) User's domain ID for connection to nova in admin context
+#   Defaults to undef
+#
 class neutron::server::placement (
   $password,
   $auth_type           = 'password',
   $username            = 'nova',
-  $project_domain_id   = 'default',
   $project_domain_name = 'Default',
   $project_name        = 'services',
-  $user_domain_id      = 'default',
   $user_domain_name    = 'Default',
   $auth_url            = 'http://127.0.0.1:5000',
   $region_name         = $::os_service_default,
   $endpoint_type       = $::os_service_default,
+  # DEPRECATED PARAMETERS
+  $project_domain_id   = undef,
+  $user_domain_id      = undef,
 ) {
 
   include neutron::deps
@@ -85,14 +87,27 @@ class neutron::server::placement (
     'placement/auth_url':            value => $auth_url;
     'placement/username':            value => $username;
     'placement/password':            value => $password, secret => true;
-    'placement/project_domain_id':   value => $project_domain_id;
     'placement/project_domain_name': value => $project_domain_name;
     'placement/project_name':        value => $project_name;
-    'placement/user_domain_id':      value => $user_domain_id;
     'placement/user_domain_name':    value => $user_domain_name;
     'placement/region_name':         value => $region_name;
     'placement/endpoint_type':       value => $endpoint_type;
     'placement/auth_type':           value => $auth_type;
   }
 
+  if $project_domain_id != undef {
+    warning('project_domain_id is deprecated and will be removed in a future release. \
+Use project_domain_name instead')
+    neutron_config {
+      'placement/project_domain_id': value => $project_domain_id;
+    }
+  }
+
+  if $user_domain_id != undef {
+    warning('user_domain_id is deprecated and will be removed in a future release. \
+Use user_domain_name instead')
+    neutron_config {
+      'placement/user_domain_id': value => $user_domain_id;
+    }
+  }
 }

releasenotes/notes/deprecate-domain_id-a836fab7c7ac17b7.yaml
@@ -1,0 +1,15 @@
+---
+features:
+  - |
+    The following parameters are now deprecated, and will be removed in a
+    future release. Please use project_domain_name and user_domain_name
+    instead.
+
+    - ``neutron::ml2::networking_baremetal::project_domain_id``
+    - ``neutron::ml2::networking_baremetal::user_domain_id``
+    - ``neutron::ml2::designate::project_domain_id``
+    - ``neutron::ml2::designate::user_domain_id``
+    - ``neutron::ml2::server::placement::project_domain_id``
+    - ``neutron::ml2::server::placement::user_domain_id``
+    - ``neutron::ml2::server::notifications::project_domain_id``
+    - ``neutron::ml2::server::notifications::user_domain_id``

spec/classes/neutron_server_notifications_spec.rb
@@ -23,10 +23,8 @@ describe 'neutron::server::notifications' do
       :auth_type                          => 'password',
       :username                           => 'nova',
       :password                           => 'secrete',
-      :project_domain_id                  => 'default',
       :project_domain_name                => 'Default',
       :project_name                       => 'services',
-      :user_domain_id                     => 'default',
       :user_domain_name                   => 'Default',
       :auth_url                           => 'http://127.0.0.1:5000',
     }
@@ -43,9 +41,9 @@ describe 'neutron::server::notifications' do
       should contain_neutron_config('nova/password').with_value('secrete')
       should contain_neutron_config('nova/password').with_secret( true )
       should contain_neutron_config('nova/region_name').with_value('<SERVICE DEFAULT>')
-      should contain_neutron_config('nova/project_domain_id').with_value('default')
+      should_not contain_neutron_config('nova/project_domain_id')
       should contain_neutron_config('nova/project_domain_name').with_value('Default')
-      should contain_neutron_config('nova/user_domain_id').with_value('default')
+      should_not contain_neutron_config('nova/user_domain_id')
       should contain_neutron_config('nova/user_domain_name').with_value('Default')
       should contain_neutron_config('nova/endpoint_type').with_value('<SERVICE DEFAULT>')
     end

spec/classes/neutron_server_placement_spec.rb
@@ -21,10 +21,8 @@ describe 'neutron::server::placement' do
       :auth_type           => 'password',
       :username            => 'nova',
       :password            => 'secrete',
-      :project_domain_id   => 'default',
       :project_domain_name => 'Default',
       :project_name        => 'services',
-      :user_domain_id      => 'default',
       :user_domain_name    => 'Default',
       :auth_url            => 'http://127.0.0.1:5000',
     }
@@ -38,9 +36,9 @@ describe 'neutron::server::placement' do
       should contain_neutron_config('placement/password').with_value('secrete')
       should contain_neutron_config('placement/password').with_secret( true )
       should contain_neutron_config('placement/region_name').with_value('<SERVICE DEFAULT>')
-      should contain_neutron_config('placement/project_domain_id').with_value('default')
+      should_not contain_neutron_config('placement/project_domain_id')
       should contain_neutron_config('placement/project_domain_name').with_value('Default')
-      should contain_neutron_config('placement/user_domain_id').with_value('default')
+      should_not contain_neutron_config('placement/user_domain_id')
       should contain_neutron_config('placement/user_domain_name').with_value('Default')
       should contain_neutron_config('placement/endpoint_type').with_value('<SERVICE DEFAULT>')
     end

commit b793bbfc699ac35b5c789410fe82bdfc1cd3d3d9

Add report_inteval parameter in ml2 baremetal
Add the parameter $report_interval to the ml2
networking-baremetal manifest. By default the global
report_interval as defined in neutron.conf is used.

In some cases it makes sense to change the report_interval
specifically for ironic-neutron-agent.

Conflicts:
  manifests/agents/ml2/networking_baremetal.pp
  spec/classes/neutron_agents_ml2_networking_baremetal_spec.rb

Backport note to stable/train:
Resolved additional conflicts caused by
dfeccd674988f34edea5ca7b95bdeea827bab000

Related-Bug: #1940838
Change-Id: I6fc0eb085c09fb1e24ff09d4f8199d2cf57dd209
(cherry picked from commit dae33092a120655dfe4b9a550ec29a8c2dba73a5)
(cherry picked from commit 41bcf66eafc092890cdd6d2560d02642c5a55761)
(cherry picked from commit 629dbe0f1e89bec38d02050f80d3dda30e20d170)
(cherry picked from commit b9f3a4baa62d72b356d3307a380f2aa1f3f7703d)

manifests/agents/ml2/networking_baremetal.pp
@@ -98,6 +98,13 @@
 #   ironic-neutron-agent config.
 #   Defaults to false.
 #
+# [*report_interval*]
+#   (optional) Set the agent report interval. By default the global report
+#   interval in neutron.conf ([agent]/report_interval) is used. This parameter
+#   can be used to override the reporting interval for the
+#   ironic-neutron-agent.
+#   Defaults to $::os_service_default
+#
 class neutron::agents::ml2::networking_baremetal (
   $password,
   $enabled             = true,
@@ -121,6 +128,7 @@ class neutron::agents::ml2::networking_baremetal (
   $retry_interval      = $::os_service_default,
   $max_retries         = $::os_service_default,
   $purge_config        = false,
+  $report_interval     = $::os_service_default,
 ) {
 
   include ::neutron::deps
@@ -154,6 +162,7 @@ class neutron::agents::ml2::networking_baremetal (
     'ironic/region_name':         value => $region_name;
     'ironic/retry_interval':      value => $retry_interval;
     'ironic/max_retries':         value => $max_retries;
+    'agent/report_interval':      value => $report_interval;
   }
 
   package { 'python-ironic-neutron-agent':

releasenotes/notes/networking-baremetal-report-interval-634d1bf90139cbeb.yaml
@@ -1,0 +1,7 @@
+---
+features:
+  - |
+    It is now possible to set the report interval for the networking-baremetal
+    ml2 agent by using new parameter ``report_interval`` in the
+    ``neutron::agents::ml2::networking_baremetal`` class.
+

spec/classes/neutron_agents_ml2_networking_baremetal_spec.rb
@@ -55,6 +55,7 @@ describe 'neutron::agents::ml2::networking_baremetal' do
       should contain_ironic_neutron_agent_config('ironic/region_name').with_value('<SERVICE DEFAULT>')
       should contain_ironic_neutron_agent_config('ironic/retry_interval').with_value('<SERVICE DEFAULT>')
       should contain_ironic_neutron_agent_config('ironic/max_retries').with_value('<SERVICE DEFAULT>')
+      should contain_ironic_neutron_agent_config('agent/report_interval').with_value('<SERVICE DEFAULT>')
     end
 
     it 'installs ironic-neutron-agent agent package' do
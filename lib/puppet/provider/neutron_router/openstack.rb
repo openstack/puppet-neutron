openstack/puppet-neutron

commit 75ca125cfaf86d3b14852a546edf9954162fd01d

Use openstack cli to manage neutron routers
... and cleans up unused implementations from the base neutron
provider.

Closes-Bug: #1808317
Change-Id: I257d55be0acfecc529f86d5fe734c6c142f64e5f

lib/puppet/provider/neutron_router/openstack.rb
@@ -1,0 +1,218 @@
+require File.join(File.dirname(__FILE__), '..','..','..',
+                  'puppet/provider/neutron')
+
+Puppet::Type.type(:neutron_router).provide(
+  :openstack,
+  :parent => Puppet::Provider::Neutron
+) do
+  desc <<-EOT
+    Neutron provider to manage neutron_router type.
+
+    Assumes that the neutron service is configured on the same host.
+  EOT
+
+  @credentials = Puppet::Provider::Openstack::CredentialsV3.new
+
+  mk_resource_methods
+
+  def initialize(value={})
+    super(value)
+    @property_flush = {}
+  end
+
+  def self.do_not_manage
+    @do_not_manage
+  end
+
+  def self.do_not_manage=(value)
+    @do_not_manage = value
+  end
+
+  def self.instances
+    self.do_not_manage = true
+    list = request('router', 'list').collect do |attrs|
+      router = request('router', 'show', attrs[:id])
+      new(
+        :ensure                 => :present,
+        :name                   => attrs[:name],
+        :id                     => attrs[:id],
+        :admin_state_up         => router[:admin_state_up],
+        :external_gateway_info  => router[:external_gateway_info],
+        :status                 => router[:status],
+        :distributed            => router[:distributed],
+        :ha                     => router[:ha],
+        :tenant_id              => router[:tenant_id],
+        :availability_zone_hint => parse_availability_zone_hint(router[:availability_zone_hints])
+      )
+    end
+    self.do_not_manage = false
+    list
+  end
+
+  def self.prefetch(resources)
+    routers = instances
+    resources.keys.each do |name|
+      if provider = routers.find{ |router| router.name == name }
+        resources[name].provider = provider
+      end
+    end
+  end
+
+  def exists?
+    @property_hash[:ensure] == :present
+  end
+
+  def create
+    if self.class.do_not_manage
+      fail("Not managing Neutron_router[#{@resource[:name]}] due to earlier Neutron API failures.")
+    end
+
+    opts = [@resource[:name]]
+
+    if @resource[:admin_state_up] == 'False'
+      opts << '--disable'
+    end
+
+    if @resource[:tenant_name]
+      opts << "--project=#{@resource[:tenant_name]}"
+    elsif @resource[:tenant_id]
+      opts << "--project=#{@resource[:tenant_id]}"
+    end
+
+    if @resource[:distributed]
+      if @resource[:distributed] == 'False'
+        opts << '--centralized'
+      else
+        opts << '--distributed'
+      end
+    end
+
+    if @resource[:ha]
+      if @resource[:ha] == 'False'
+        opts << '--no-ha'
+      else
+        opts << '--ha'
+      end
+    end
+
+    if @resource[:availability_zone_hint]
+      opts << \
+        "--availability-zone-hint=#{@resource[:availability_zone_hint]}"
+    end
+
+    router = self.class.request('router', 'create', opts)
+
+    if @resource[:gateway_network_id]
+      self.class.request('router', 'set',
+                         [@resource[:name],
+                          "--external-gateway=#{@resource[:gateway_network_id]}"])
+      router = self.class.request('router', 'show', [@resource[:name]])
+    elsif @resource[:gateway_network_name]
+      self.class.request('router', 'set',
+                         [@resource[:name],
+                          "--external-gateway=#{@resource[:gateway_network_name]}"])
+      router = self.class.request('router', 'show', [@resource[:name]])
+    end
+
+    @property_hash = {
+      :ensure                 => :present,
+      :name                   => router[:name],
+      :id                     => router[:id],
+      :admin_state_up         => router[:admin_state_up],
+      :external_gateway_info  => router[:external_gateway_info],
+      :status                 => router[:status],
+      :distributed            => router[:distributed],
+      :ha                     => router[:ha],
+      :tenant_id              => router[:tenant_id],
+      :availability_zone_hint => self.class.parse_availability_zone_hint(router[:availability_zone_hints])
+    }
+  end
+
+  def flush
+    if !@property_flush.empty?
+      opts = [@resource[:name]]
+      clear_opts = [@resource[:name]]
+
+      if @property_flush.has_key?(:admin_state_up)
+        if @property_flush[:admin_state_up] == 'False'
+          opts << '--disable'
+        else
+          opts << '--enable'
+        end
+      end
+
+      if @property_flush.has_key?(:distributed)
+        if @property_flush[:distributed] == 'False'
+          opts << '--centralized'
+        else
+          opts << '--distributed'
+        end
+      end
+
+      if @property_flush.has_key?(:gateway_network_id)
+        if @property_flush[:gateway_network_id] == ''
+          clear_opts << '--external-gateway'
+        else
+          opts << "--external-gateway=#{@property_flush[:gateway_network_id]}"
+        end
+      elsif @property_flush.has_key?(:gateway_network_name)
+        if @property_flush[:gateway_network_name] == ''
+          clear_opts << '--external-gateway'
+        else
+          opts << "--external-gateway=#{@property_flush[:gateway_network_name]}"
+        end
+      end
+
+      if @property_flush.has_key?(:ha)
+        if @property_flush[:ha] == 'False'
+          opts << '--no-ha'
+        else
+          opts << '--ha'
+        end
+      end
+
+      if clear_opts.length > 1
+        self.class.request('router', 'unset', clear_opts)
+      end
+      if opts.length > 1
+        self.class.request('router', 'set', opts)
+      end
+      @property_flush.clear
+    end
+  end
+
+  def destroy
+    if self.class.do_not_manage
+      fail("Not managing Neutron_router[#{@resource[:name]}] due to earlier Neutron API failures.")
+    end
+    self.class.request('router', 'delete', @resource[:name])
+    @property_flush.clear
+    @property_flush[:ensure] = :absent
+  end
+
+  [
+    :admin_state_up,
+    :gateway_network_id,
+    :gateway_network_name,
+    :distributed,
+    :ha,
+  ].each do |attr|
+    define_method(attr.to_s + "=") do |value|
+      if self.class.do_not_manage
+        fail("Not managing Neutron_router[#{@resource[:name]}] due to earlier Neutron API failures.")
+      end
+      @property_flush[attr] = value
+    end
+  end
+
+  [
+    :availability_zone_hint,
+    :tenant_id,
+    :tenant_name,
+  ].each do |attr|
+    define_method(attr.to_s + "=") do |value|
+      fail("Property #{attr.to_s} does not support being updated")
+    end
+  end
+
+end

spec/unit/provider/neutron_router/openstack_spec.rb
@@ -1,0 +1,352 @@
+require 'puppet'
+require 'spec_helper'
+require 'puppet/provider/neutron_router/openstack'
+
+provider_class = Puppet::Type.type(:neutron_router).provider(:openstack)
+
+describe provider_class do
+
+  let(:set_env) do
+    ENV['OS_USERNAME']     = 'test'
+    ENV['OS_PASSWORD']     = 'abc123'
+    ENV['OS_PROJECT_NAME'] = 'test'
+    ENV['OS_AUTH_URL']     = 'http://127.0.0.1:5000'
+  end
+
+  describe 'manage routers' do
+    let :router_name do
+      'router1'
+    end
+
+    let :router_attrs do
+      {
+        :name   => router_name,
+        :ensure => 'present',
+      }
+    end
+
+    let :resource do
+      Puppet::Type::Neutron_router.new(router_attrs)
+    end
+
+    let :provider do
+      provider_class.new(resource)
+    end
+
+    before :each do
+      set_env
+    end
+
+    describe '#create' do
+      context 'with defaults' do
+        it 'creates router' do
+          provider_class.expects(:openstack)
+            .with('router', 'create', '--format', 'shell',
+                  ['router1'])
+            .returns('admin_state_up="True"
+availability_zone_hints="[]"
+distributed="False"
+external_gateway_info="None"
+ha="True"
+id="d73f453a-77ca-4843-977a-3af0fda8dfcb"
+name="router1"
+project_id="60f9544eb94c42a6b7e8e98c2be981b1"
+project_id="60f9544eb94c42a6b7e8e98c2be981b1"
+status="ACTIVE"')
+          provider.create
+          expect(provider.exists?).to be_truthy
+          expect(provider.admin_state_up).to eq('True')
+          expect(provider.ha).to eq('True')
+          expect(provider.distributed).to eq('False')
+          expect(provider.status).to eq('ACTIVE')
+        end
+      end
+
+      context 'with admin_state_up' do
+        let :router_attrs do
+          {
+            :name           => router_name,
+            :ensure         => 'present',
+            :admin_state_up => 'False',
+          }
+        end
+        it 'creates router' do
+          provider_class.expects(:openstack)
+            .with('router', 'create', '--format', 'shell',
+                  ['router1', '--disable'])
+            .returns('admin_state_up="False"
+availability_zone_hints="[]"
+distributed="False"
+external_gateway_info="None"
+ha="True"
+id="d73f453a-77ca-4843-977a-3af0fda8dfcb"
+name="router1"
+project_id="60f9544eb94c42a6b7e8e98c2be981b1"
+status="ACTIVE"')
+          provider.create
+          expect(provider.exists?).to be_truthy
+          expect(provider.admin_state_up).to eq('False')
+        end
+      end
+
+      context 'with centralized' do
+        let :router_attrs do
+          {
+            :name        => router_name,
+            :ensure      => 'present',
+            :distributed => 'False',
+          }
+        end
+        it 'creates router' do
+          provider_class.expects(:openstack)
+            .with('router', 'create', '--format', 'shell',
+                  ['router1', '--centralized'])
+            .returns('admin_state_up="True"
+availability_zone_hints="[]"
+distributed="False"
+external_gateway_info="None"
+ha="True"
+id="d73f453a-77ca-4843-977a-3af0fda8dfcb"
+name="router1"
+project_id="60f9544eb94c42a6b7e8e98c2be981b1"
+status="ACTIVE"')
+          provider.create
+          expect(provider.exists?).to be_truthy
+          expect(provider.distributed).to eq('False')
+        end
+      end
+
+      context 'with distributed' do
+        let :router_attrs do
+          {
+            :name        => router_name,
+            :ensure      => 'present',
+            :distributed => 'True',
+          }
+        end
+        it 'creates router' do
+          provider_class.expects(:openstack)
+            .with('router', 'create', '--format', 'shell',
+                  ['router1', '--distributed'])
+            .returns('admin_state_up="True"
+availability_zone_hints="[]"
+distributed="True"
+external_gateway_info="None"
+ha="True"
+id="d73f453a-77ca-4843-977a-3af0fda8dfcb"
+name="router1"
+project_id="60f9544eb94c42a6b7e8e98c2be981b1"
+status="ACTIVE"')
+          provider.create
+          expect(provider.exists?).to be_truthy
+          expect(provider.distributed).to eq('True')
+        end
+      end
+
+      context 'with ha' do
+        let :router_attrs do
+          {
+            :name   => router_name,
+            :ensure => 'present',
+            :ha     => 'True',
+          }
+        end
+        it 'creates router' do
+          provider_class.expects(:openstack)
+            .with('router', 'create', '--format', 'shell',
+                  ['router1', '--ha'])
+            .returns('admin_state_up="True"
+availability_zone_hints="[]"
+distributed="False"
+external_gateway_info="None"
+ha="True"
+id="d73f453a-77ca-4843-977a-3af0fda8dfcb"
+name="router1"
+project_id="60f9544eb94c42a6b7e8e98c2be981b1"
+status="ACTIVE"')
+          provider.create
+          expect(provider.exists?).to be_truthy
+          expect(provider.ha).to eq('True')
+        end
+      end
+
+      context 'with non-ha' do
+        let :router_attrs do
+          {
+            :name   => router_name,
+            :ensure => 'present',
+            :ha     => 'False',
+          }
+        end
+        it 'creates router' do
+          provider_class.expects(:openstack)
+            .with('router', 'create', '--format', 'shell',
+                  ['router1', '--no-ha'])
+            .returns('admin_state_up="True"
+availability_zone_hints="[]"
+distributed="False"
+external_gateway_info="None"
+ha="False"
+id="d73f453a-77ca-4843-977a-3af0fda8dfcb"
+name="router1"
+project_id="60f9544eb94c42a6b7e8e98c2be981b1"
+status="ACTIVE"')
+          provider.create
+          expect(provider.exists?).to be_truthy
+          expect(provider.ha).to eq('False')
+        end
+      end
+
+      context 'with gateway_network_name' do
+        let :router_attrs do
+          {
+            :name                 => router_name,
+            :ensure               => 'present',
+            :gateway_network_name => 'net1',
+          }
+        end
+        it 'creates router' do
+          provider_class.expects(:openstack)
+            .with('router', 'create', '--format', 'shell',
+                  ['router1'])
+            .returns('admin_state_up="True"
+availability_zone_hints="[]"
+distributed="False"
+external_gateway_info="None"
+ha="False"
+id="d73f453a-77ca-4843-977a-3af0fda8dfcb"
+name="router1"
+project_id="60f9544eb94c42a6b7e8e98c2be981b1"
+status="ACTIVE"')
+          provider_class.expects(:openstack)
+            .with('router', 'set', ['router1', '--external-gateway=net1'])
+          provider_class.expects(:openstack)
+            .with('router', 'show', '--format', 'shell',
+                  ['router1'])
+            .returns('admin_state_up="True"
+availability_zone_hints="[]"
+distributed="False"
+external_gateway_info="{\'network_id\': \'076520cc-b783-4cf5-a4a9-4cb5a5e93a9b\'}"
+ha="False"
+id="d73f453a-77ca-4843-977a-3af0fda8dfcb"
+name="router1"
+project_id="60f9544eb94c42a6b7e8e98c2be981b1"
+status="ACTIVE"')
+          provider.create
+          expect(provider.exists?).to be_truthy
+        end
+      end
+    end
+
+    describe '#destroy' do
+      it 'removes router' do
+        provider_class.expects(:openstack)
+          .with('router', 'delete', 'router1')
+        provider.destroy
+        expect(provider.exists?).to be_falsey
+      end
+    end
+
+    describe '#flush' do
+      context '.admin_state_up' do
+        it 'updates router' do
+          provider_class.expects(:openstack)
+            .with('router', 'set', ['router1', '--disable'])
+          provider.admin_state_up = 'False'
+          provider.flush
+          provider_class.expects(:openstack)
+            .with('router', 'set', ['router1', '--enable'])
+          provider.admin_state_up = 'True'
+          provider.flush
+        end
+      end
+      context '.distributed' do
+        it 'updates router' do
+          provider_class.expects(:openstack)
+            .with('router', 'set', ['router1', '--distributed'])
+          provider.distributed = 'True'
+          provider.flush
+          provider_class.expects(:openstack)
+            .with('router', 'set', ['router1', '--centralized'])
+          provider.distributed = 'False'
+          provider.flush
+        end
+      end
+      context '.ha' do
+        it 'updates router' do
+          provider_class.expects(:openstack)
+            .with('router', 'set', ['router1', '--ha'])
+          provider.ha = 'True'
+          provider.flush
+          provider_class.expects(:openstack)
+            .with('router', 'set', ['router1', '--no-ha'])
+          provider.ha = 'False'
+          provider.flush
+        end
+      end
+      context '.gateway_network_name' do
+        it 'updates router' do
+          provider_class.expects(:openstack)
+            .with('router', 'set', ['router1', '--external-gateway=net1'])
+          provider.gateway_network_name = 'net1'
+          provider.flush
+          provider_class.expects(:openstack)
+            .with('router', 'unset', ['router1', '--external-gateway'])
+          provider.gateway_network_name = ''
+          provider.flush
+        end
+      end
+    end
+
+    describe '#instances' do
+      it 'lists router' do
+        provider_class.expects(:openstack)
+          .with('router', 'list', '--quiet', '--format', 'csv', [])
+          .returns('"ID","Name","Status","State","Project","Distributed","HA"
+"d73f453a-77ca-4843-977a-3af0fda8dfcb","router1","ACTIVE","True","60f9544eb94c42a6b7e8e98c2be981b1",True,False
+"c3e93a5b-45ee-4029-b3a3-3331cb3e3f2a","router2","DOWN","False","60f9544eb94c42a6b7e8e98c2be981b1",False,True
+')
+        provider_class.expects(:openstack)
+          .with('router', 'show', '--format', 'shell', 'd73f453a-77ca-4843-977a-3af0fda8dfcb')
+          .returns('admin_state_up="True"
+availability_zone_hints="[]"
+distributed="False"
+external_gateway_info="None"
+ha="True"
+id="d73f453a-77ca-4843-977a-3af0fda8dfcb"
+name="router1"
+project_id="60f9544eb94c42a6b7e8e98c2be981b1"
+status="ACTIVE"')
+        provider_class.expects(:openstack)
+          .with('router', 'show', '--format', 'shell', 'c3e93a5b-45ee-4029-b3a3-3331cb3e3f2a')
+          .returns('admin_state_up="False"
+availability_zone_hints="[]"
+distributed="True"
+external_gateway_info="None"
+ha="False"
+id="c3e93a5b-45ee-4029-b3a3-3331cb3e3f2a"
+name="router2"
+project_id="60f9544eb94c42a6b7e8e98c2be981b1"
+project_id="60f9544eb94c42a6b7e8e98c2be981b1"
+status="DOWN"')
+
+        instances = provider_class.instances
+        expect(instances.length).to eq(2)
+
+        expect(instances[0].id).to eq('d73f453a-77ca-4843-977a-3af0fda8dfcb')
+        expect(instances[0].name).to eq('router1')
+        expect(instances[0].admin_state_up).to eq('True')
+        expect(instances[0].ha).to eq('True')
+        expect(instances[0].distributed).to eq('False')
+        expect(instances[0].status).to eq('ACTIVE')
+
+        expect(instances[1].id).to eq('c3e93a5b-45ee-4029-b3a3-3331cb3e3f2a')
+        expect(instances[1].name).to eq('router2')
+        expect(instances[1].admin_state_up).to eq('False')
+        expect(instances[1].ha).to eq('False')
+        expect(instances[1].distributed).to eq('True')
+        expect(instances[1].status).to eq('DOWN')
+      end
+    end
+  end
+end

lib/puppet/provider/neutron_network/openstack.rb
@@ -167,15 +167,6 @@ Puppet::Type.type(:neutron_network).provide(
     @property_hash[:ensure] = :absent
   end
 
-  def self.parse_availability_zone_hint(value)
-    hints = JSON.parse(value.gsub(/\\"/,'"').gsub('u\'', '"').gsub('\'','"'))
-    if hints.length > 1
-      hints
-    else
-      hints.first
-    end
-  end
-
   [
     :admin_state_up,
     :shared,

lib/puppet/provider/neutron.rb
@@ -13,7 +13,6 @@ class Puppet::Provider::Neutron < Puppet::Provider::Openstack
   extend Puppet::Provider::Openstack::Auth
 
   initvars
-  commands :neutron => 'neutron'
 
   def self.request(service, action, properties=nil)
     begin
@@ -40,20 +39,6 @@ class Puppet::Provider::Neutron < Puppet::Provider::Openstack
 
   def self.conf_filename
     '/etc/neutron/neutron.conf'
-  end
-
-  def self.withenv(hash, &block)
-    saved = ENV.to_hash
-    hash.each do |name, val|
-      ENV[name.to_s] = val
-    end
-
-    yield
-  ensure
-    ENV.clear
-    saved.each do |name, val|
-      ENV[name] = val
-    end
   end
 
   def self.neutron_conf
@@ -113,58 +98,6 @@ class Puppet::Provider::Neutron < Puppet::Provider::Openstack
     @auth_endpoint ||= get_auth_endpoint
   end
 
-  def self.auth_neutron(*args)
-    q = neutron_credentials
-    authenv = {
-      :OS_AUTH_URL            => self.auth_endpoint,
-      :OS_USERNAME            => q['username'],
-      :OS_PROJECT_NAME        => q['project_name'],
-      :OS_PASSWORD            => q['password'],
-      :OS_PROJECT_DOMAIN_NAME => q['project_domain_name'],
-      :OS_USER_DOMAIN_NAME    => q['user_domain_name']
-    }
-    if q.key?('region_name')
-      authenv[:OS_REGION_NAME] = q['region_name']
-    end
-    rv = nil
-    timeout = 10
-    end_time = Time.now.to_i + timeout
-    loop do
-      begin
-        withenv authenv do
-          rv = neutron(args)
-        end
-        break
-      rescue Puppet::ExecutionFailure => e
-        if ! e.message =~ /(\(HTTP\s+400\))|
-              (400-\{\'message\'\:\s+\'\'\})|
-              (\[Errno 111\]\s+Connection\s+refused)|
-              (503\s+Service\s+Unavailable)|
-              (504\s+Gateway\s+Time-out)|
-              (\:\s+Maximum\s+attempts\s+reached)|
-              (Unauthorized\:\s+bad\s+credentials)|
-              (Max\s+retries\s+exceeded)/
-          raise(e)
-        end
-        current_time = Time.now.to_i
-        if current_time > end_time
-          break
-        else
-          wait = end_time - current_time
-          notice("Unable to complete neutron request due to non-fatal error: \"#{e.message}\". Retrying for #{wait} sec.")
-        end
-        sleep(2)
-        # Note(xarses): Don't remove, we know that there is one of the
-        # Recoverable erros above, So we will retry a few more times
-      end
-    end
-    return rv
-  end
-
-  def auth_neutron(*args)
-    self.class.auth_neutron(args)
-  end
-
   def self.reset
     @neutron_conf        = nil
     @neutron_credentials = nil
@@ -194,82 +127,12 @@ class Puppet::Provider::Neutron < Puppet::Provider::Openstack
     end
   end
 
-  def self.list_neutron_resources(type)
-    ids = []
-    list = cleanup_csv_with_id(auth_neutron("#{type}-list", '--format=csv',
-                                            '--column=id', '--quote=none'))
-    if list.nil?
-      raise(Puppet::ExecutionFailure, "Can't retrieve #{type}-list because Neutron or Keystone API is not available.")
-    end
-
-    (list.split("\n")[1..-1] || []).compact.collect do |line|
-      ids << line.strip
-    end
-    return ids
-  end
-
-  def self.get_neutron_resource_attrs(type, id)
-    attrs = {}
-    net = auth_neutron("#{type}-show", '--format=shell', id)
-    if net.nil?
-      raise(Puppet::ExecutionFailure, "Can't retrieve #{type}-show because Neutron or Keystone API is not available.")
-    end
-
-    last_key = nil
-    (net.split("\n") || []).compact.collect do |line|
-      if line.include? '='
-        k, v = line.split('=', 2)
-        attrs[k] = v.gsub(/\A"|"\Z/, '')
-        last_key = k
-      else
-        # Handle the case of a list of values
-        v = line.gsub(/\A"|"\Z/, '')
-        attrs[last_key] = [attrs[last_key], v].flatten
-      end
-    end
-    return attrs
-  end
-
-  def self.get_tenant_id(catalog, name, domain='Default')
-    instance_type = 'keystone_tenant'
-    instance = catalog.resource("#{instance_type.capitalize!}[#{name}]")
-    if ! instance
-      instance = Puppet::Type.type(instance_type).instances.find do |i|
-        # We need to check against the Default domain name because of
-        # https://review.opendev.org/#/c/226919/ which changed the naming
-        # format for the tenant to include <Domain name>. This should be
-        # removed when we drop the resource without a domain name.
-        # TODO(aschultz): remove ::domain lookup as part of M-cycle
-        i.provider.name == name || i.provider.name == "#{name}::#{domain}"
-      end
-    end
-    if instance
-      return instance.provider.id
+  def self.parse_availability_zone_hint(value)
+    hints = JSON.parse(value.gsub(/\\"/,'"').gsub('u\'', '"').gsub('\'','"'))
+    if hints.length > 1
+      hints
     else
-      fail("Unable to find #{instance_type} for name #{name}")
+      hints.first
     end
-  end
-
-  def self.parse_creation_output(data)
-    hash = {}
-    data.split("\n").compact.each do |line|
-      if line.include? '='
-        hash[line.split('=').first] = line.split('=', 2)[1].gsub(/\A"|"\Z/, '')
-      end
-    end
-    hash
-  end
-
-  def self.cleanup_csv(text)
-    # Ignore warnings - assume legitimate output starts with a double quoted
-    # string.  Errors will be caught and raised prior to this
-    text = text.split("\n").drop_while { |line| line !~ /^\".*\"/ }.join("\n")
-    "#{text}\n"
-  end
-
-  def self.cleanup_csv_with_id(text)
-    return nil if text.nil?
-    text = text.split("\n").drop_while { |line| line !~ /^\s*id$/ }.join("\n")
-    "#{text}\n"
   end
 end

commit 2ef08f70b152258af763e33fff16f326a49dfeb1

neutron_router: Fix detection of tenant_id
This change fixes the wrong key used to pick up tenant_id, which was
introduced by 75ca125cfaf86d3b14852a546edf9954162fd01d .

Change-Id: Icb0d67a70f940182c69e3a877e69425f75f98135

lib/puppet/provider/neutron_router/openstack.rb
@@ -41,7 +41,7 @@ Puppet::Type.type(:neutron_router).provide(
         :status                 => router[:status],
         :distributed            => router[:distributed],
         :ha                     => router[:ha],
-        :tenant_id              => router[:tenant_id],
+        :tenant_id              => router[:project_id],
         :availability_zone_hint => parse_availability_zone_hint(router[:availability_zone_hints])
       )
     end
@@ -123,7 +123,7 @@ Puppet::Type.type(:neutron_router).provide(
       :status                 => router[:status],
       :distributed            => router[:distributed],
       :ha                     => router[:ha],
-      :tenant_id              => router[:tenant_id],
+      :tenant_id              => router[:project_id],
       :availability_zone_hint => self.class.parse_availability_zone_hint(router[:availability_zone_hints])
     }
   end

spec/unit/provider/neutron_router/openstack_spec.rb
@@ -51,7 +51,6 @@ ha="True"
 id="d73f453a-77ca-4843-977a-3af0fda8dfcb"
 name="router1"
 project_id="60f9544eb94c42a6b7e8e98c2be981b1"
-project_id="60f9544eb94c42a6b7e8e98c2be981b1"
 status="ACTIVE"')
           provider.create
           expect(provider.exists?).to be_truthy
@@ -326,7 +325,6 @@ external_gateway_info="None"
 ha="False"
 id="c3e93a5b-45ee-4029-b3a3-3331cb3e3f2a"
 name="router2"
-project_id="60f9544eb94c42a6b7e8e98c2be981b1"
 project_id="60f9544eb94c42a6b7e8e98c2be981b1"
 status="DOWN"')
 
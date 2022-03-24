b35ceea55a0201fc59dc986e444593c4fbe37d70
Takashi Kajinami <tkajinam@redhat.com>
2022-03-25 00:04:35 +0900

    Contrail: Deprecate [KEYSTONE] parameters
    ... because these were removed a long ago and no longer used[1].
    Currently the [keystone_authtoken] parameters are used instead.
    
    [1] https://github.com/Juniper/contrail-neutron-plugin/commit/4e91f8040
    
    Change-Id: I5ce8a429282c546b3e3bcd3f9a761d7812bacddf

--- manifests/plugins/opencontrail.pp
+++ manifests/plugins/opencontrail.pp
@@ -3,19 +3,19 @@
 # === Parameters
 #
 # [*api_server_ip*]
-#   IP address of the API Server
+#   (Optional) IP address of the API Server
 #   Defaults to $::os_service_default
 #
 # [*api_server_port*]
-#   Port of the API Server.
+#   (Optional) Port of the API Server.
 #   Defaults to $::os_service_default
 #
 # [*multi_tenancy*]
-#   Whether to enable multi-tenancy
+#   (Optional) Whether to enable multi-tenancy
 #   Default to $::os_service_default
 #
 # [*contrail_extensions*]
-#   Array of OpenContrail extensions to be supported
+#   (Optional) Array of OpenContrail extensions to be supported
 #   Defaults to $::os_service_default
 #   Example:
 #
@@ -23,51 +23,66 @@
 #       contrail_extensions => ['ipam:neutron_plugin_contrail.plugins.opencontrail.contrail_plugin_ipam.NeutronPluginContrailIpam']
 #     }
 #
-# [*keystone_auth_url*]
-#   Url of the keystone auth server
-#   Defaults to $::os_service_default
-#
-# [*keystone_admin_user*]
-#   Admin user name
-#   Defaults to $::os_service_default
-#
-# [*keystone_admin_tenant_name*]
-#   Admin_tenant_name
-#   Defaults to $::os_service_default
-#
-# [*keystone_admin_password*]
-#   Admin password
-#   Defaults to $::os_service_default
-#
-# [*keystone_admin_token*]
-#   Admin token
-#   Defaults to $::os_service_default
-#
 # [*package_ensure*]
-#   (optional) Ensure state for package.
+#   (Optional) Ensure state for package.
 #   Defaults to 'present'.
 #
 # [*purge_config*]
-#   (optional) Whether to set only the specified config options
+#   (Optional) Whether to set only the specified config options
 #   in the opencontrail config.
 #   Defaults to false.
+#
+# DEPRECATED PARAMETERS
+#
+# [*keystone_auth_url*]
+#   Url of the keystone auth server
+#   Defaults to undef
+#
+# [*keystone_admin_user*]
+#   Admin user name
+#   Defaults to undef
+#
+# [*keystone_admin_tenant_name*]
+#   Admin_tenant_name
+#   Defaults to undef
+#
+# [*keystone_admin_password*]
+#   Admin password
+#   Defaults to undef
+#
+# [*keystone_admin_token*]
+#   Admin token
+#   Defaults to undef
 #
 class neutron::plugins::opencontrail (
   $api_server_ip              = $::os_service_default,
   $api_server_port            = $::os_service_default,
   $multi_tenancy              = $::os_service_default,
   $contrail_extensions        = $::os_service_default,
-  $keystone_auth_url          = $::os_service_default,
-  $keystone_admin_user        = $::os_service_default,
-  $keystone_admin_tenant_name = $::os_service_default,
-  $keystone_admin_password    = $::os_service_default,
-  $keystone_admin_token       = $::os_service_default,
   $package_ensure             = 'present',
   $purge_config               = false,
+  # DEPRECATED PARAMETERS
+  $keystone_auth_url          = undef,
+  $keystone_admin_user        = undef,
+  $keystone_admin_tenant_name = undef,
+  $keystone_admin_password    = undef,
+  $keystone_admin_token       = undef,
 ) {
 
   include neutron::deps
   include neutron::params
+
+  [
+    'keystone_auth_url',
+    'keystone_admin_user',
+    'keystone_admin_tenant_name',
+    'keystone_admin_password',
+    'keystone_admin_token'
+  ].each |String $key_opt| {
+    if getvar($key_opt) != undef {
+      warning("The ${key_opt} parameter is deprecated and has no effect.")
+    }
+  }
 
   validate_legacy(Array, 'validate_array', $contrail_extensions)
 
@@ -111,11 +126,14 @@ class neutron::plugins::opencontrail (
     'APISERVER/api_server_port':     value => $api_server_port;
     'APISERVER/multi_tenancy':       value => $multi_tenancy;
     'APISERVER/contrail_extensions': value => join($contrail_extensions, ',');
-    'KEYSTONE/auth_url':             value => $keystone_auth_url;
-    'KEYSTONE/admin_user' :          value => $keystone_admin_user;
-    'KEYSTONE/admin_tenant_name':    value => $keystone_admin_tenant_name;
-    'KEYSTONE/admin_password':       value => $keystone_admin_password, secret =>true;
-    'KEYSTONE/admin_token':          value => $keystone_admin_token, secret =>true;
+  }
+
+  neutron_plugin_opencontrail {
+    'KEYSTONE/auth_url':          ensure => absent;
+    'KEYSTONE/admin_user' :       ensure => absent;
+    'KEYSTONE/admin_tenant_name': ensure => absent;
+    'KEYSTONE/admin_password':    ensure => absent;
+    'KEYSTONE/admin_token':       ensure => absent;
   }
 
 }

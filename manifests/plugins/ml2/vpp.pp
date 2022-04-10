71cf56485a6eaad733efe006ee8fe84236bee242
Takashi Kajinami <tkajinam@redhat.com>
2022-04-10 23:44:51 +0900

    networking-vpp: Support l3 parameters
    This change introduces support for the following two parameters of
    the vpp plugin, so that users can customize L3 feature of the plugin.
     [ml2_vpp] l3_hosts
     [ml2_vpp] enable_l3_ha
    
    Closes-Bug: #1790526
    Change-Id: I0545fa84429718f579f5386ee85fb9671b618ce3

--- manifests/plugins/ml2/vpp.pp
+++ manifests/plugins/ml2/vpp.pp
@@ -5,34 +5,46 @@
 # === Parameters
 #
 # [*etcd_host*]
-# (optional) etcd server host name or IP.
-# Defaults to $::os_service_default
+#   (optional) etcd server host name or IP.
+#   Defaults to $::os_service_default
 #
 # [*etcd_port*]
-# (optional) etcd server listening port.
-# Defaults to $::os_service_default.
+#   (optional) etcd server listening port.
+#   Defaults to $::os_service_default.
 #
 # [*etcd_user*]
-# (optional) User name for etcd authentication
-# Defaults to $::os_service_default.
+#   (optional) User name for etcd authentication
+#   Defaults to $::os_service_default.
 #
 # [*etcd_pass*]
-# (optional) Password for etcd authentication
-# Defaults to $::os_service_default.
+#   (optional) Password for etcd authentication
+#   Defaults to $::os_service_default.
+#
+# [*l3_hosts*]
+#   (optional) List of hostnames to render L3 services on.
+#   Defaults to $::os_service_default.
+#
+# [*enable_l3_ha*]
+#   (optional) Enable L3 HA feature.
+#   Defaults to $::os_service_default.
 #
 class neutron::plugins::ml2::vpp (
-  $etcd_host = $::os_service_default,
-  $etcd_port = $::os_service_default,
-  $etcd_user = $::os_service_default,
-  $etcd_pass = $::os_service_default,
+  $etcd_host    = $::os_service_default,
+  $etcd_port    = $::os_service_default,
+  $etcd_user    = $::os_service_default,
+  $etcd_pass    = $::os_service_default,
+  $l3_hosts     = $::os_service_default,
+  $enable_l3_ha = $::os_service_default,
 ) {
   include neutron::deps
   require neutron::plugins::ml2
 
   neutron_plugin_ml2 {
-    'ml2_vpp/etcd_host': value => $etcd_host;
-    'ml2_vpp/etcd_port': value => $etcd_port;
-    'ml2_vpp/etcd_user': value => $etcd_user;
-    'ml2_vpp/etcd_pass': value => $etcd_pass, secret => true;
+    'ml2_vpp/etcd_host':    value => $etcd_host;
+    'ml2_vpp/etcd_port':    value => $etcd_port;
+    'ml2_vpp/etcd_user':    value => $etcd_user;
+    'ml2_vpp/etcd_pass':    value => $etcd_pass, secret => true;
+    'ml2_vpp/l3_hosts':     value => join(any2array($l3_hosts), ',');
+    'ml2_vpp/enable_l3_ha': value => $enable_l3_ha;
   }
 }
--- spec/classes/neutron_plugins_ml2_vpp_spec.rb
+++ spec/classes/neutron_plugins_ml2_vpp_spec.rb
@@ -10,38 +10,42 @@ describe 'neutron::plugins::ml2::vpp' do
       core_plugin     => 'neutron.plugins.ml2.plugin.Ml2Plugin' }"
   end
 
-  let :default_params do
-    {
-      :etcd_host => '127.0.0.1',
-      :etcd_port => 4001,
-    }
-  end
-
   let :params do
-    {
-    }
+    {}
   end
 
   shared_examples 'neutron plugin vpp ml2' do
-    before do
-      params.merge!(default_params)
-    end
 
-    it 'configures ml2_vpp settings' do
-      should contain_neutron_plugin_ml2('ml2_vpp/etcd_host').with_value(params[:etcd_host])
-      should contain_neutron_plugin_ml2('ml2_vpp/etcd_port').with_value(params[:etcd_port])
-      should contain_neutron_plugin_ml2('ml2_vpp/etcd_user').with_value('<SERVICE DEFAULT>')
-      should contain_neutron_plugin_ml2('ml2_vpp/etcd_pass').with_value('<SERVICE DEFAULT>').with_secret(true)
-    end
-
-    context 'when enabling etcd authentication' do
-      before :each do
-        params.merge!(:etcd_user => 'admin',
-                      :etcd_pass => 'password' )
+    context 'with defaults' do
+      it 'configures ml2_vpp settings' do
+        should contain_neutron_plugin_ml2('ml2_vpp/etcd_host').with_value('<SERVICE DEFAULT>')
+        should contain_neutron_plugin_ml2('ml2_vpp/etcd_port').with_value('<SERVICE DEFAULT>')
+        should contain_neutron_plugin_ml2('ml2_vpp/etcd_user').with_value('<SERVICE DEFAULT>')
+        should contain_neutron_plugin_ml2('ml2_vpp/etcd_pass').with_value('<SERVICE DEFAULT>').with_secret(true)
+        should contain_neutron_plugin_ml2('ml2_vpp/l3_hosts').with_value('<SERVICE DEFAULT>')
+        should contain_neutron_plugin_ml2('ml2_vpp/enable_l3_ha').with_value('<SERVICE DEFAULT>')
       end
-      it 'should configure etcd username and password' do
+    end
+
+    context 'with parameters' do
+      before :each do
+        params.merge!({
+          :etcd_host    => '127.0.0.1',
+          :etcd_port    => 4001,
+          :etcd_user    => 'admin',
+          :etcd_pass    => 'password',
+          :l3_hosts     => ['192.0.2.10', '192.0.2.11'],
+          :enable_l3_ha => false,
+        })
+      end
+
+      it 'configures ml2_vpp settings' do
+        should contain_neutron_plugin_ml2('ml2_vpp/etcd_host').with_value('127.0.0.1')
+        should contain_neutron_plugin_ml2('ml2_vpp/etcd_port').with_value(4001)
         should contain_neutron_plugin_ml2('ml2_vpp/etcd_user').with_value('admin')
         should contain_neutron_plugin_ml2('ml2_vpp/etcd_pass').with_value('password').with_secret(true)
+        should contain_neutron_plugin_ml2('ml2_vpp/l3_hosts').with_value('192.0.2.10,192.0.2.11')
+        should contain_neutron_plugin_ml2('ml2_vpp/enable_l3_ha').with_value(false)
       end
     end
   end

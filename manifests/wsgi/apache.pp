386dd3c488194dfba7862b1d57f70f152eaaa284
Takashi Kajinami <tkajinam@redhat.com>
2022-08-02 20:38:17 +0900

    Adapt to new type validation in puppetlabs-apache
    The puppetlabs-apache module is enforcing more strict data type
    validation[1].
    
    This change updates the default values to adapt to that change.
    
    [1] https://github.com/puppetlabs/puppetlabs-apache/commit/f41251e336fa3e7c31c19968ccee74121cf71e47
    
    Closes-Bug: #1983300
    Depends-on: https://review.opendev.org/851652
    Change-Id: I91fc9238dd5209bf22381a1171b9f580a9a4e7f9

--- manifests/wsgi/apache.pp
+++ manifests/wsgi/apache.pp
@@ -48,7 +48,7 @@
 #
 #   [*priority*]
 #     (optional) The priority for the vhost.
-#     Defaults to '10'
+#     Defaults to 10
 #
 #   [*threads*]
 #     (optional) The number of threads for the vhost.
@@ -70,11 +70,11 @@
 #
 #   [*access_log_file*]
 #     The log file name for the virtualhost.
-#     Optional. Defaults to false.
+#     Optional. Defaults to undef.
 #
 #   [*access_log_format*]
 #     The log format for the virtualhost.
-#     Optional. Defaults to false.
+#     Optional. Defaults to undef.
 #
 #   [*error_log_file*]
 #     The error log file name for the virtualhost.
@@ -122,9 +122,9 @@ class neutron::wsgi::apache (
   $ssl_certs_dir               = undef,
   $wsgi_process_display_name   = undef,
   $threads                     = 1,
-  $priority                    = '10',
-  $access_log_file             = false,
-  $access_log_format           = false,
+  $priority                    = 10,
+  $access_log_file             = undef,
+  $access_log_format           = undef,
   $error_log_file              = undef,
   $custom_wsgi_process_options = {},
   $headers                     = undef,

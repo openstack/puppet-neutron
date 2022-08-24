openstack/puppet-neutron

commit db295da3d6ffc5c3065c9e52a5c8eb3d448ee32b

Fix wrong package name of networking-mlnx
This change ensures python3- package is used instead of python-
package, because python- package is no longer available since default
python in distros switched to Python 3.

Closes-Bug: #1968476
Change-Id: Iec165853471ce0e0bdc07df14983846f489d64d6

manifests/agents/ml2/mlnx.pp
@@ -77,9 +77,9 @@ class neutron::agents::ml2::mlnx (
   include neutron::deps
   include neutron::params
 
-  $mlnx_agent_package          = $::neutron::params::mlnx_agent_package
-  $mlnx_agent_service          = $::neutron::params::mlnx_agent_service
-  $eswitchd_service            = $::neutron::params::eswitchd_service
+  $mlnx_agent_package = $::neutron::params::mlnx_agent_package
+  $mlnx_agent_service = $::neutron::params::mlnx_agent_service
+  $eswitchd_service   = $::neutron::params::eswitchd_service
 
   neutron_mlnx_agent_config {
     'eswitch/physical_interface_mappings': value => pick(join(any2array($physical_interface_mappings), ','), $::os_service_default);
@@ -108,10 +108,15 @@ class neutron::agents::ml2::mlnx (
   }
 
   if $manage_package {
-    package { $mlnx_agent_package:
+    ensure_packages($mlnx_agent_package, {
       ensure => $package_ensure,
-      tag    => ['openstack', 'neutron-package'],
-    }
+      tag    => ['openstack'],
+    })
+
+    # NOTE(tkajinam): CentOS/RHEL uses the same package for both agent and
+    #                 plugin. This is required to avoid conflict with
+    #                 neutron::plugins::ml2::mellanox
+    Package<| title == $mlnx_agent_package |> { tag +> 'neutron-package' }
   }
 
   if $manage_service {

manifests/plugins/ml2/mellanox.pp
@@ -24,10 +24,15 @@ class neutron::plugins::ml2::mellanox (
     fail("Unsupported osfamily ${::osfamily}")
   }
 
-  ensure_resource('package', 'python-networking-mlnx',
-    {
-      ensure => $package_ensure,
-      tag    => ['openstack', 'neutron-plugin-ml2-package']
-    }
-  )
+  $mlnx_plugin_package = $::neutron::params::mlnx_plugin_package
+
+  ensure_packages($mlnx_plugin_package, {
+    ensure => $package_ensure,
+    tag    => ['openstack'],
+  })
+
+  # NOTE(tkajinam): CentOS/RHEL uses the same package for both agent and
+  #                 plugin. This is required to avoid conflict with
+  #                 neutron::agens::ml2::mlnx
+  Package<| title == $mlnx_plugin_package |> { tag +> 'neutron-plugin-ml2-package' }
 }

spec/classes/neutron_agents_ml2_mlnx_spec.rb
@@ -5,22 +5,11 @@ describe 'neutron::agents::ml2::mlnx' do
     "class { 'neutron': }"
   end
 
-  let :default_params do
-    {
-      :package_ensure => 'present',
-      :enabled        => true,
-    }
-  end
-
   let :params do
     {}
   end
 
   shared_examples 'neutron mlnx agent with ml2 plugin' do
-    let :p do
-      default_params.merge(params)
-    end
-
     it { should contain_class('neutron::params') }
 
 
@@ -32,7 +21,7 @@ describe 'neutron::agents::ml2::mlnx' do
     it 'installs neutron mlnx agent package' do
       should contain_package(platform_params[:mlnx_agent_package]).with(
         :name   => platform_params[:mlnx_agent_package],
-        :ensure => p[:package_ensure],
+        :ensure => 'installed',
         :tag    => ['openstack', 'neutron-package'],
       )
       should contain_package(platform_params[:mlnx_agent_package]).that_requires('Anchor[neutron::install::begin]')

manifests/params.pp
@@ -31,6 +31,7 @@ class neutron::params {
   $user                        = 'neutron'
   $group                       = 'neutron'
   $mlnx_agent_package          = 'python3-networking-mlnx'
+  $mlnx_plugin_package         = 'python3-networking-mlnx'
   $eswitchd_service            = 'eswitchd'
 
   if($::osfamily == 'Redhat') {

spec/classes/neutron_plugins_ml2_mellanox_spec.rb
@@ -11,26 +11,16 @@ describe 'neutron::plugins::ml2::mellanox' do
      }"
   end
 
-  let :default_params do
-    {
-      :package_ensure => 'present'
-    }
-  end
-
   let :params do
     {}
   end
 
   shared_examples 'neutron plugin mellanox ml2' do
-    before do
-      params.merge!(default_params)
-    end
-
     it { should contain_class('neutron::params') }
 
     it 'should have' do
-      should contain_package('python-networking-mlnx').with(
-        :ensure => params[:package_ensure],
+      should contain_package(platform_params[:mlnx_plugin_package]).with(
+        :ensure => 'installed',
         :tag    => ['openstack', 'neutron-plugin-ml2-package']
         )
     end
@@ -42,6 +32,19 @@ describe 'neutron::plugins::ml2::mellanox' do
     context "on #{os}" do
       let (:facts) do
         facts.merge!(OSDefaults.get_facts())
+      end
+
+      let (:platform_params) do
+        case facts[:osfamily]
+        when 'Debian'
+          {
+            :mlnx_plugin_package => 'python3-networking-mlnx',
+          }
+        when 'RedHat'
+          {
+            :mlnx_plugin_package => 'python3-networking-mlnx',
+          }
+        end
       end
 
       if facts[:osfamily] == 'RedHat'

commit 2ed7911ac7e1cc0007a12a434d5a7273771d9b32

mlnx: Fix circular dependencies
This change fixes the following circular dependencies caused by
the commit db295da3d6ffc5c3065c9e52a5c8eb3d448ee32b , which fixed
the wrong plugin package name.

Anchor[neutron::config::begin]
  => File[/etc/neutron/plugin.ini]
  => Class[Neutron::Plugins::Ml2]
  => Class[Neutron::Plugins::Ml2::Mellanox]
  => Package[python3-networking-mlnx]
  => Anchor[neutron::install::end]
  => Anchor[neutron::config::begin]

Change-Id: I397172ad44ebe9e0c096a0bcc15781c1be2a2486

manifests/agents/ml2/mlnx.pp
@@ -77,10 +77,11 @@ class neutron::agents::ml2::mlnx (
   include neutron::deps
   include neutron::params
 
-  $mlnx_agent_package = $::neutron::params::mlnx_agent_package
-  $mlnx_agent_service = $::neutron::params::mlnx_agent_service
-  $eswitchd_package   = $::neutron::params::eswitchd_package
-  $eswitchd_service   = $::neutron::params::eswitchd_service
+  $mlnx_agent_package  = $::neutron::params::mlnx_agent_package
+  $mlnx_agent_service  = $::neutron::params::mlnx_agent_service
+  $eswitchd_package    = $::neutron::params::eswitchd_package
+  $eswitchd_service    = $::neutron::params::eswitchd_service
+  $mlnx_plugin_package = $::neutron::params::mlnx_plugin_package
 
   neutron_mlnx_agent_config {
     'eswitch/physical_interface_mappings': value => pick(join(any2array($physical_interface_mappings), ','), $::os_service_default);
@@ -109,15 +110,16 @@ class neutron::agents::ml2::mlnx (
   }
 
   if $manage_package {
+    if $mlnx_agent_package != $mlnx_plugin_package {
+      $mlnx_agent_package_tag = ['openstack', 'neutron-package']
+    } else {
+      $mlnx_agent_package_tag = ['openstack', 'neutron-plugin-ml2-package']
+      Package[$mlnx_agent_package] -> Neutron_mlnx_agent_config<||>
+    }
     ensure_packages($mlnx_agent_package, {
       ensure => $package_ensure,
-      tag    => ['openstack'],
+      tag    => $mlnx_agent_package_tag,
     })
-
-    # NOTE(tkajinam): CentOS/RHEL uses the same package for both agent and
-    #                 plugin. This is required to avoid conflict with
-    #                 neutron::plugins::ml2::mellanox
-    Package<| title == $mlnx_agent_package |> { tag +> 'neutron-package' }
 
     # NOTE(tkajinam): Ubuntu/Debian requires a separate package for eswitchd
     #                 service.

manifests/plugins/ml2/mellanox.pp
@@ -23,11 +23,6 @@ class neutron::plugins::ml2::mellanox (
 
   ensure_packages($mlnx_plugin_package, {
     ensure => $package_ensure,
-    tag    => ['openstack'],
+    tag    => ['openstack', 'neutron-plugin-ml2-package'],
   })
-
-  # NOTE(tkajinam): CentOS/RHEL uses the same package for both agent and
-  #                 plugin. This is required to avoid conflict with
-  #                 neutron::agens::ml2::mlnx
-  Package<| title == $mlnx_plugin_package |> { tag +> 'neutron-plugin-ml2-package' }
 }

spec/classes/neutron_agents_ml2_mlnx_spec.rb
@@ -22,7 +22,7 @@ describe 'neutron::agents::ml2::mlnx' do
       should contain_package(platform_params[:mlnx_agent_package]).with(
         :name   => platform_params[:mlnx_agent_package],
         :ensure => 'installed',
-        :tag    => ['openstack', 'neutron-package'],
+        :tag    => platform_params[:mlnx_agent_package_tag]
       )
 
       if platform_params[:eswitchd_package]
@@ -109,17 +109,19 @@ describe 'neutron::agents::ml2::mlnx' do
         case facts[:osfamily]
         when 'Debian'
           {
-            :mlnx_agent_package => 'neutron-mlnx-agent',
-            :mlnx_agent_service => 'neutron-mlnx-agent',
-            :eswitchd_package   => 'networking-mlnx-eswitchd',
-            :eswitchd_service   => 'networking-mlnx-eswitchd',
+            :mlnx_agent_package     => 'neutron-mlnx-agent',
+            :mlnx_agent_service     => 'neutron-mlnx-agent',
+            :eswitchd_package       => 'networking-mlnx-eswitchd',
+            :eswitchd_service       => 'networking-mlnx-eswitchd',
+            :mlnx_agent_package_tag => ['openstack', 'neutron-package'],
           }
         when 'RedHat'
           {
-            :mlnx_agent_package => 'python3-networking-mlnx',
-            :mlnx_agent_service => 'neutron-mlnx-agent',
-            :eswitchd_package   => false,
-            :eswitchd_service   => 'eswitchd',
+            :mlnx_agent_package     => 'python3-networking-mlnx',
+            :mlnx_agent_service     => 'neutron-mlnx-agent',
+            :eswitchd_package       => false,
+            :eswitchd_service       => 'eswitchd',
+            :mlnx_agent_package_tag => ['openstack', 'neutron-plugin-ml2-package'],
           }
         end
       end
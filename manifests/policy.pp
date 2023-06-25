55d36ea5d83a3d2c9f37db9a3aeca43ae8fc5d98
Tobias Urdin <tobias.urdin@binero.com>
2023-06-25 23:55:47 +0200

    Add per module policy service refresh
    Updating the policies for this project should only
    refresh the services that reads it.
    
    Change-Id: I541a7dbb11580155cf2bb3af0ffbf6ebc5b1220b
    (cherry picked from commit f7f43194782035c58c6fd799fe08136068a4dedc)
    (cherry picked from commit d76f4b4dfd6ee9d4f641c726c0da86b2a7373d61)

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -62,6 +62,7 @@ class neutron::policy (
     file_group   => $::neutron::params::group,
     file_format  => 'yaml',
     purge_config => $purge_config,
+    tag          => 'neutron',
   }
 
   create_resources('openstacklib::policy', { $policy_path => $policy_parameters })

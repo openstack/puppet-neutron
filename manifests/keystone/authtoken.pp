416df9fc39f89e4fe5da22fb1b75fdf4c4563031
Takashi Kajinami <tkajinam@redhat.com>
2023-07-12 22:03:41 +0900

    authtoken: Make password required
    The password parameter is not really optional. This makes it
    a required parameter to give more sensible validation error.
    
    Change-Id: Ieb74be9b8736113950363862f953ae783a62683f

--- manifests/keystone/authtoken.pp
+++ manifests/keystone/authtoken.pp
@@ -4,13 +4,12 @@
 #
 # === Parameters
 #
+# [*password*]
+#   (Required) Password to create for the service user
+#
 # [*username*]
 #   (Optional) The name of the service user
 #   Defaults to 'neutron'
-#
-# [*password*]
-#   (Optional) Password to create for the service user
-#   Defaults to $facts['os_service_default']
 #
 # [*auth_url*]
 #   (Optional) The URL to use for authentication.
@@ -196,8 +195,8 @@
 #  authtoken class. Values set here override the individual parameters above.
 #
 class neutron::keystone::authtoken(
+  String[1] $password,
   $username                       = 'neutron',
-  $password                       = $facts['os_service_default'],
   $auth_url                       = 'http://localhost:5000',
   $project_name                   = 'services',
   $user_domain_name               = 'Default',
@@ -237,10 +236,6 @@ class neutron::keystone::authtoken(
 ) {
 
   include neutron::deps
-
-  if is_service_default($password) {
-    fail('Please set password for neutron service user')
-  }
 
   keystone::resource::authtoken {
     'neutron_config':

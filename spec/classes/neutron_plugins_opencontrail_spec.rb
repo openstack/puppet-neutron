d70cabeb526c17dc1e4ce1966ee275e48b521b3c
Takashi Kajinami <tkajinam@redhat.com>
2023-07-25 23:24:04 +0900

    Accept a string and hash for contrail_extensions
    ... following the logic we have for DictOpt options.
    
    Change-Id: I20204bc6c63336d2e04d3e19a058e8b2f23b0778

--- spec/classes/neutron_plugins_opencontrail_spec.rb
+++ spec/classes/neutron_plugins_opencontrail_spec.rb
@@ -2,37 +2,61 @@ require 'spec_helper'
 
 describe 'neutron::plugins::opencontrail' do
   shared_examples 'neutron::plugins::contrail' do
-    let :params do
-      {
-        :api_server_ip       => '10.0.0.1',
-        :api_server_port     => '8082',
-        :contrail_extensions => ['ipam:ipam','policy:policy','route-table'],
-        :purge_config        => false,
-      }
+
+    context 'with defaults' do
+      it 'passes purge to resource' do
+        should contain_resources('neutron_plugin_opencontrail').with({
+          :purge => false
+        })
+      end
+
+      it 'should perform default configuration of' do
+        should contain_neutron_plugin_opencontrail('APISERVER/api_server_ip').with_value('<SERVICE DEFAULT>')
+        should contain_neutron_plugin_opencontrail('APISERVER/api_server_port').with_value('<SERVICE DEFAULT>')
+        should contain_neutron_plugin_opencontrail('APISERVER/contrail_extensions').with_value('<SERVICE DEFAULT>')
+        should contain_neutron_plugin_opencontrail('APISERVER/timeout').with_value('<SERVICE DEFAULT>')
+        should contain_neutron_plugin_opencontrail('APISERVER/connection_timeout').with_value('<SERVICE DEFAULT>')
+      end
     end
 
-    it 'passes purge to resource' do
-      should contain_resources('neutron_plugin_opencontrail').with({
-        :purge => false
-      })
+    context 'with parameters' do
+      let :params do
+        {
+          :api_server_ip       => '10.0.0.1',
+          :api_server_port     => '8082',
+          :contrail_extensions => ['ipam:ipam','policy:policy','route-table'],
+          :timeout             => 10,
+          :connection_timeout  => 5,
+        }
+      end
+
+      it 'should set the given values' do
+        should contain_neutron_plugin_opencontrail('APISERVER/api_server_ip').with_value(params[:api_server_ip])
+        should contain_neutron_plugin_opencontrail('APISERVER/api_server_port').with_value(params[:api_server_port])
+        should contain_neutron_plugin_opencontrail('APISERVER/contrail_extensions').with_value(params[:contrail_extensions].join(','))
+        should contain_neutron_plugin_opencontrail('APISERVER/timeout').with_value(params[:timeout])
+        should contain_neutron_plugin_opencontrail('APISERVER/connection_timeout').with_value(params[:connection_timeout])
+      end
     end
 
-    it 'should perform default configuration of' do
-      should contain_neutron_plugin_opencontrail('APISERVER/api_server_ip').with_value(params[:api_server_ip])
-      should contain_neutron_plugin_opencontrail('APISERVER/api_server_port').with_value(params[:api_server_port])
-      should contain_neutron_plugin_opencontrail('APISERVER/contrail_extensions').with_value(params[:contrail_extensions].join(','))
-      should contain_neutron_plugin_opencontrail('APISERVER/timeout').with_value('<SERVICE DEFAULT>')
-      should contain_neutron_plugin_opencontrail('APISERVER/connection_timeout').with_value('<SERVICE DEFAULT>')
+    context 'contrail_extensions set by a dictionary' do
+      let :params do
+        {
+          :contrail_extensions => {
+            'ipam'        => 'ipam',
+            'policy'      => 'policy',
+            'route-table' => 'route-table',
+          }
+        }
+      end
+
+      it 'should configure the contrail_extensions' do
+        should contain_neutron_plugin_opencontrail('APISERVER/contrail_extensions').with_value('ipam:ipam,policy:policy,route-table:route-table')
+      end
     end
   end
 
   shared_examples 'neutron::plugins::opencontrail on Debian' do
-    let :params do
-      {
-        :contrail_extensions => ['ipam:ipam','policy:policy','route-table']
-      }
-    end
-
     it 'configures /etc/default/neutron-server' do
       should contain_file_line('/etc/default/neutron-server:NEUTRON_PLUGIN_CONFIG').with(
         :path    => '/etc/default/neutron-server',
@@ -46,12 +70,6 @@ describe 'neutron::plugins::opencontrail' do
   end
 
   shared_examples 'neutron::plugins::opencontrail on RedHat' do
-    let :params do
-      {
-        :contrail_extensions => ['ipam:ipam','policy:policy','route-table']
-      }
-    end
-
     it 'should create plugin symbolic link' do
       should contain_file('/etc/neutron/plugin.ini').with(
         :ensure  => 'link',

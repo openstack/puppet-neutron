b1e18bf6ff8734c0cffa58ebcc6b13d8c0c7d47c
Takashi Kajinami <kajinamit@oss.nttdata.com>
2023-11-06 23:49:47 +0900

    Remove deprecated neutron::enabled
    This parameter was deprecated during 2023.1 cycle[1].
    
    [1] 7a5be0030d31f62c31387a3756c9e7b795d00ac7
    
    Change-Id: If2bfbd71d0aa6ed5742516422e9fa1dc06b9304a

--- manifests/init.pp
+++ manifests/init.pp
@@ -323,11 +323,6 @@
 #   (optional) Allow plugins that support it to create VLAN transparent networks
 #   Defaults to $facts['os_service_default']
 #
-# DEPRECATED PARAMETERS
-#
-# [*enabled*]
-#   (required) Whether or not to enable the neutron service
-#
 class neutron (
   $package_ensure                       = 'present',
   $bind_host                            = $facts['os_service_default'],
@@ -397,16 +392,10 @@ class neutron (
   $notification_transport_url           = $facts['os_service_default'],
   $max_allowed_address_pair             = $facts['os_service_default'],
   $vlan_transparent                     = $facts['os_service_default'],
-  # DEPRECATED PARAMETERS
-  $enabled                              = undef,
 ) {
 
   include neutron::deps
   include neutron::params
-
-  if $enabled != undef {
-    warning('The neutron::enabled parameter has been deprecated and has no effect')
-  }
 
   if ! is_service_default($use_ssl) and ($use_ssl) {
     if is_service_default($cert_file) {

77191ec66ac61e1f79d91b2d48f968ae8542800f
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-01-30 00:44:48 +0900

    Add support for oslo.reports options
    Depends-on: https://review.opendev.org/804804
    Change-Id: I00c1dd458eac202fa1250bf0299ecf35ab6bb6e6

---
+++ manifests/reports.pp
@@ -0,0 +1,33 @@
+# == Class: neutron::reports
+#
+# Configure oslo_reports options
+#
+# === Parameters
+#
+# [*log_dir*]
+#   (Optional) Path to a log directory where to create a file
+#   Defaults to $facts['os_service_default']
+#
+# [*file_event_handler*]
+#   (Optional) The path to a file to watch for changes to trigger the reports.
+#   Defaults to $facts['os_service_default']
+#
+# [*file_event_handler_interval*]
+#   (Optional) How many seconds to wait between pools when file_event_handler
+#   is set.
+#   Defaults to $facts['os_service_default']
+#
+class neutron::reports(
+  $log_dir                     = $facts['os_service_default'],
+  $file_event_handler          = $facts['os_service_default'],
+  $file_event_handler_interval = $facts['os_service_default'],
+) {
+
+  include neutron::deps
+
+  oslo::reports { 'neutron_config':
+    log_dir                     => $log_dir,
+    file_event_handler          => $file_event_handler,
+    file_event_handler_interval => $file_event_handler_interval,
+  }
+}

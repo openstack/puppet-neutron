7d299d93bc71412a4443e8c26769e2306a361e89
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-02-07 22:40:23 +0900

    Add support for [ovn] live_migration_activation_strategy
    Depends-on: https://review.opendev.org/938106
    Change-Id: I202fbe275232d43eaad8939faa4d560f343e82f9

--- manifests/plugins/ml2/ovn.pp
+++ manifests/plugins/ml2/ovn.pp
@@ -142,6 +142,10 @@
 #   the router through any routers.
 #   Defaults to $facts['os_service_default']
 #
+# [*live_migration_activation_strategy*]
+#   (optional) Activation strategy to use for live migration.
+#   Defaults to $facts['os_service_default']
+#
 # [*ignore_lsp_down*]
 #   (optional) Do not install ARP/ND reply flows for logical switch ports
 #   if the port is DOWN.
@@ -212,6 +216,7 @@ class neutron::plugins::ml2::ovn(
   $mac_binding_age_threshold            = $facts['os_service_default'],
   $broadcast_arps_to_all_routers        = $facts['os_service_default'],
   $ovn_router_indirect_snat             = $facts['os_service_default'],
+  $live_migration_activation_strategy   = $facts['os_service_default'],
   $ignore_lsp_down                      = $facts['os_service_default'],
   $fdb_removal_limit                    = $facts['os_service_default'],
   $mac_binding_removal_limit            = $facts['os_service_default'],
@@ -270,6 +275,7 @@ class neutron::plugins::ml2::ovn(
     'ovn/mac_binding_age_threshold'           : value => $mac_binding_age_threshold;
     'ovn/broadcast_arps_to_all_routers'       : value => $broadcast_arps_to_all_routers;
     'ovn/ovn_router_indirect_snat'            : value => $ovn_router_indirect_snat;
+    'ovn/live_migration_activation_strategy'  : value => $live_migration_activation_strategy;
     'ovn_nb_global/ignore_lsp_down'           : value => $ignore_lsp_down;
     'ovn_nb_global/fdb_removal_limit'         : value => $fdb_removal_limit;
     'ovn_nb_global/mac_binding_removal_limit' : value => $mac_binding_removal_limit;

83b7410ad0f5d0d2832c3ef6983e3000d31a2302
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-02-25 01:36:25 +0900

    Fix assertion of heartbeat_in_pthread parameter
    Its default in puppet-oslo has been changed by [1].
    
    [1] 06a9b12199ed03842bb935749dedf4fc8e20d043
    
    Change-Id: Iafd3bc7f6711766863198dbb09835884ba740915

--- spec/classes/neutron_init_spec.rb
+++ spec/classes/neutron_init_spec.rb
@@ -64,7 +64,7 @@ describe 'neutron' do
       should contain_oslo__messaging__rabbit('neutron_config').with(
         :heartbeat_timeout_threshold          => '<SERVICE DEFAULT>',
         :heartbeat_rate                       => '<SERVICE DEFAULT>',
-        :heartbeat_in_pthread                 => '<SERVICE DEFAULT>',
+        :heartbeat_in_pthread                 => nil,
         :rabbit_qos_prefetch_count            => '<SERVICE DEFAULT>',
         :rabbit_use_ssl                       => '<SERVICE DEFAULT>',
         :rabbit_transient_queues_ttl          => '<SERVICE DEFAULT>',
